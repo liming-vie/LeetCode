427373e571e54f534cfae267020c04180f8602d9
liming-vie <liming.vie@gmail.com>
2016-04-25 20:57:39 +0800

    4.Median of Two Sorted Arrays; 2016/04/25; Do bipartition to k, just like binary search, make it to O(k)

--- MedianOfTwoSortedArrays.cpp
+++ MedianOfTwoSortedArrays.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
-  double findkth(int A[], int m, int B[], int n, int k) {
-    if (m > n)  return findkth(B, n, A, m, k);
-    if (m == 0) return B[k - 1];
-    if (k == 1) return (B[0] < A[0] ? B[0] : A[0]);
-    int pa = k >> 1;
-    if (pa>m) pa = m;
-    int pb = k - pa;
-    if (A[pa - 1] < B[pb - 1])
-      return findkth(A+pa, m - pa, B, n, k - pa);
-    else if (A[pa - 1] > B[pb - 1])
-      return findkth(A, m, B+pb, n - pb, k - pb);
-    else return A[pa - 1];
-  }
-
-  double findMedianSortedArrays(int A[], int m, int B[], int n) {
-    if ((m + n) & 1)  return findkth(A, m, B, n, ((m + n) >> 1) + 1);
-    else return (findkth(A, m, B, n, (m + n) >> 1) + findkth(A, m, B, n, ((m + n) >> 1) + 1)) / 2;
-  }
-
+    double findKthNumber(int* nums1, int size1, int* nums2, int size2, int k) {
+        if(size1 > size2)   return findKthNumber(nums2, size2, nums1, size1, k);
+        if(size1 == 0)  return nums2[k-1];
+        if(k==1)    return *nums1 < *nums2 ? *nums1 : *nums2;
+        int mid1 = k >> 1;
+        mid1 = mid1 > size1 ? size1 : mid1;
+        int mid2 = k-mid1;
+        int n1 = nums1[mid1-1], n2 = nums2[mid2-1];
+        if(n1 < n2) 
+            return findKthNumber(nums1+mid1, size1-mid1, nums2, size2, k-mid1);
+        if(n1 > n2) 
+            return findKthNumber(nums1, size1, nums2+mid2, size2-mid2, k-mid2);
+        return n1;
+    }
+    
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int s1 = nums1.size();
+        int s2 = nums2.size();
+        int mid = (s1+s2)>>1;
+        if((s1 + s2) & 1){
+            return findKthNumber(nums1.data(), s1, nums2.data(), s2, mid + 1);
+        }
+        return (findKthNumber(nums1.data(), s1, nums2.data(), s2, mid + 1) + findKthNumber(nums1.data(), s1, nums2.data(), s2, mid)) / 2;
+    }
 };
