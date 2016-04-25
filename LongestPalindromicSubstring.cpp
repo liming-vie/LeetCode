ab3ed34e3dd88fe758380f51e2a59b41427bfa14
liming-vie <liming.vie@gmail.com>
2016-04-25 22:21:43 +0800

    5. Longest Palindromic Substring; use dynamic programming, O(n^2), not good enough; 2016/04/25

--- LongestPalindromicSubstring.cpp
+++ LongestPalindromicSubstring.cpp
@@ -1,34 +1,25 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int len=s.length();
-        int res=0,tmp;
-        int rl;
-        for(int i=0; i<len; ++i){
-            int l=i-1,r=i+1;
-            tmp=1;
-            while(l>=0 && r<len && s[l]==s[r]){
-               tmp+=2;
-               l--;
-               r++;
-            }  
-            if(tmp>res){
-                res=tmp;
-                rl = l+1;
-            }  
-            l=i-1;
-            r=i;
-            tmp=0;
-            while(l>=0 && r<len && s[l]==s[r]){
-                tmp+=2;
-                l--;
-                r++;
-            }
-            if(tmp>res){
-                res=tmp;
-                rl=l+1;
-            } 
+        int len = s.length();
+        if(len < 2) return s;
+        bool is[1000][1000];
+        for(int i=0; i<len; ++i) {
+            is[i][i] = true;
         }
-        return s.substr(rl,res);
+        int res=1,res_b=0;
+        for(int i=1; i<len; ++i) {
+            is[i-1][i] = (s[i] == s[i-1]) ? res=2, res_b=i-1, true: false;
+        }
+        
+        int upperi, j;
+        for(int l=3; l<=len; ++l) {
+            upperi = len - l;
+            for(int i=0; i<=upperi; ++i) {
+                j=i+l-1;
+                is[i][j] = ((s[i] == s[j]) && is[i+1][j-1]) ? res=l,res_b=i,true : false;
+            }
+        }
+        return s.substr(res_b, res);
     }
 };
