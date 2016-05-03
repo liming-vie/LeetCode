72cb79edb9f813b787ec6900b307526bd7845909
liming-vie <liming.vie@gmail.com>
2016-05-03 14:15:33 +0800

    5. Longest Palindromic Substring; use Manacher’s algorithm to reduce time to O(n)

--- LongestPalindromicSubstring.cpp
+++ LongestPalindromicSubstring.cpp
@@ -1,53 +1,45 @@
+// learn from http://articles.leetcode.com/longest-palindromic-substring-part-ii
 class Solution {
 public:
-    int*next;
-    void computeNext(string&pattern, int begin) {
-        int i=0;
-        int j=-1;
-        int idx=0;
-        next[0]=-1;
-        int len=pattern.length() - begin;
-        while(i<len) {
-            while(j!=-1 && pattern[j] != pattern[idx])
-                j=next[j];
-            next[++i] = ++j;
-            ++idx;
+    string preProcess(string&s) {
+        int len=s.length();
+        if(len==0)  return "^$";
+        // add ^ and $ to avoid boundary check
+        stringstream ss;
+        ss << "^";
+        for(int i=0; i<len; ++i) {
+            ss << '#' << s[i];
         }
-    }
-    int func(string&s, string&p, int begin_p) {
-        int i=0,j=0;
-        int l1=s.length(), l2=p.length();
-        int res=0;
-        while(i<l1 && j < l2) {
-            if(j==-1 || s[i]==p[j+begin_p]) {
-                ++i;
-                ++j;
-                res = res > j ? res : j;
-            }
-            else {
-                j=next[j];
-            }
-        }
-        return res;
+        ss << "#$";
+        return ss.str();
     }
     string longestPalindrome(string s) {
-        int len=s.length();
-        if(len<2)   return s;
-        
-        next = new int[len+1];
-        int res=0;
-        int begin=0;
-        string rev = s;
-        reverse(rev.begin(), rev.end());
-        for(int i=0; i<len; ++i) {
-            computeNext(rev, i);
-            int tmp=func(s,rev, i);
-            if(tmp>res) {
-                res=tmp;
-                begin=i;
+        string str = preProcess(s);
+        int len=str.length();
+        int *p = new int[len];
+        int center=0,right=0;
+        int mirror;
+        int upper = len-1;
+        for(int i=1; i<upper; ++i) {
+            mirror = (center << 1) - i;
+            p[i] = (right>i) ? min(right-i, p[mirror]) : 0;
+            // expand length
+            while(str[i+p[i]+1] == str[i-p[i]-1])
+                ++p[i];
+            if(p[i] + i > right) {
+                right = p[i] + i;
+                center=i;
             }
-            if(len - i < res) break;
         }
-        return rev.substr(begin, res);
+        
+        right=0;
+        center=0;
+        for(int i=1; i<upper; ++i) {
+            if(p[i] > right) {
+                right=p[i];
+                center = i;
+            }
+        }
+        return s.substr((center-right-1)>>1 ,right);
     }
 };
