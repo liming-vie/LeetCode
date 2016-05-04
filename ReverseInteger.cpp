9487fd8c37002707b38f97da513df304df4e85d0
liming-vie <liming.vie@gmail.com>
2016-05-04 22:31:55 +0800

    7. Reverse Integer; Simpler code; in 8ms; 2016/05/04

--- ReverseInteger.cpp
+++ ReverseInteger.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
     int reverse(int x) {
-        bool neg= (x<0);
-        long long int tmp=x;
-        if(neg) tmp = 0-tmp;
-        char res[100];
-        int n=0;
-        long long int k=1;
-        while(tmp>0){
-            res[n++]=tmp%10;
-            tmp/=10;
-            k*=10;
+        bool negative = false;
+        if(x<0) {
+            negative = true;
+            x = -x;
         }
-        
-        long long int resl=0;
-        for(int i=0; i<n; ++i){
-             k/=10;   
-             resl+=res[i]*k;
+        long long ret = 0;
+        while(x) {
+            ret = ret * 10 + x % 10;
+            x /= 10;
         }
-        if(neg){
-            resl=0-resl;  
-        } 
-        if(resl < (1<<31) || resl > (~(1<<31))) return 0;
-        return resl;
+        if(negative)    ret = -ret;
+        return (ret > (~(1<<31)) || ret < (1<<31)) ? 0 : ret;
     }
 };
