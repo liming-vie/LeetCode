d50b3ab09a1d8a7fd19e15d1d46df64a8c624939
liming-vie <liming.vie@gmail.com>
2016-05-04 22:07:39 +0800

    6..ZigZag Conversion; I wonder if all the quickest answers are faster because of inline???

--- ZigZagConversion.cpp
+++ ZigZagConversion.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    string convert(string s, int numRows) {
+    inline string convert(string s, int numRows) {
         if(numRows == 1)    return s;
         int len=s.length();
         int max = (numRows << 1) - 2;
         int d;
         int j=0;
-        stringstream ss;
+        string ret;
         for(int i=0; i<numRows; ++i) {
             d = max - (i<<1);
             d = (d==0 ? max : d);
             j=i;
             while(j<len) {
-                ss<<s[j];
+                ret += s[j];
                 j+=d;
                 d = (d==max?max:max-d);
             }
         }
-        return ss.str();
+        return ret;
     }
 };
