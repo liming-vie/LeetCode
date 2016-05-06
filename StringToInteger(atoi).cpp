b2ee8a23a0750c50f94dec6953aa3e8e99f88bd5
liming-vie <liming.vie@gmail.com>
2016-05-06 23:33:30 +0800

    8. String to Integer (atoi); use l to quick check overflow, make it 8ms; 2016/05/06

--- StringToInteger(atoi).cpp
+++ StringToInteger(atoi).cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
-    int atoi(string str) {
-        int MAX=2147483647;
-        int MIN=-2147483648;
+    int myAtoi(string str) {
+        int MIN=1<<31;
+        int MAX=~MIN;
+        
         int len=str.length();
-        
-        long long res=0;
-        int i=0;
-        bool neg=false;
-        int nl=0;
-        
-        while(str[i]==' ') ++i;
-        
-        if(str[i]=='-'){
-          neg=true;
-          ++i;
-        }else if(str[i]=='+') ++i;
-        
-        
-        for(; i<len; ++i,++nl){
-            if(str[i] <'0' || str[i]>'9')  break;
-            res *= 10;
-            res += str[i]-'0';
+        int idx=0;
+        while(idx<len && str[idx]==' ') ++idx;
+        long result=0;
+        bool negative = false;
+        if(str[idx] == '-') {
+            negative=true;
+            ++idx;
         }
-        
-        if(neg) res = 0-res;
-        if(!neg && (nl > 10 || res>=MAX)) return MAX;
-        if(nl>10 || res<=MIN) return MIN;
-        return res;
+        else if(str[idx] == '+')
+            ++idx;
+            
+        long res=0;
+        int l=0;
+        while(idx<len) {
+            if(str[idx]<'0' || str[idx]>'9') {
+                break;
+            }
+            res = res*10+(str[idx]-'0');
+            ++idx;
+            if(++l==11) return negative?MIN:MAX;
+        }
+        res = negative?-res:res;
+        return res < MIN ? MIN : (res > MAX ? MAX : res);
     }
 };
