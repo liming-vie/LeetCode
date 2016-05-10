9d949d278491194d0a1e92dbb280f28a8fd8168e
liming-vie <liming.vie@gmail.com>
2016-05-11 00:19:14 +0800

    10. Regular Expression Matching; Use dp with O(n^2) in space and time

--- RegularExpressionMatching.cpp
+++ RegularExpressionMatching.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
-    bool match(const char*s,const char*p) {
-        if(*p=='\0')    return *s == '\0';
-        if(*(p+1) != '*') {
-            return (*p == *s || (*p=='.' && *s!='\0')) && match(s+1,p+1);
+    inline bool isMatch(string s, string p) {
+        int l1=s.length(),l2=p.length();
+        vector<vector<bool>>dp(l1+1,vector<bool>(l2+1,false));
+        dp[0][0]=true;
+        for(int i=1; i<=l1; ++i){
+            dp[i][0] = false;
         }
-        while(*p == *s || (*p=='.' && *s!='\0')) {
-            if(match(s,p+2))    return true;
-            ++s;
+        for(int j=1; j<=l2; ++j) {
+            dp[0][j] = p[j-1]=='*' ? dp[0][j-2] : false;
         }
-        return match(s,p+2);
-    }
-    bool isMatch(string s, string p) {
-        return match(s.c_str(), p.c_str());
+        for(int i=1; i<=l1; ++i) {
+            for(int j=1; j<=l2; ++j) {
+                if(p[j-1] == '*') {
+                    dp[i][j] = dp[i][j-1] || dp[i][j-2] || (dp[i-1][j] && (p[j-2]=='.'||p[j-2]==s[i-1]));
+                }
+                else {
+                    dp[i][j] = dp[i-1][j-1] && (p[j-1]=='.' || p[j-1] == s[i-1]);
+                }
+            }
+        }
+        return dp[l1][l2];
     }
 };
