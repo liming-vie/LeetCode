cfbf9214cc0b1e71c532f5c4527fa3b7aeef6ca8
liming-vie <liming.vie@gmail.com>
2016-05-10 23:12:22 +0800

    10. Regular Expression Matching; 2016/05/10

--- RegularExpressionMatching.cpp
+++ RegularExpressionMatching.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
-bool func(const char*s, const char*p, int n1, int n2){
-  if (n1 == n2 && n1 == 0)  return true;
-  if (n1 < 0 || n2 < 0 || (n2==0 && n1!=0)) return false;
-  
-  if (n2>1 && *(p + 1) == '*'){
-    if (*p == '.' || (n1>0 && *p == *s))
-      return func(s + 1, p, n1 - 1, n2) || func(s, p + 2, n1, n2 - 2);
-    return func(s, p + 2, n1, n2 - 2);
-  }
-  else{
-    if ((n1 > 0 && *p == *s) || *p == '.')
-      return func(s + 1, p + 1, n1 - 1, n2 - 1);
-    return false;
-  }
-}
-
-    bool isMatch(const char *s, const char *p) {
-        int len1=strlen(s);
-        int len2=strlen(p);
-        return func(s,p,len1,len2);
+    bool match(const char*s,const char*p) {
+        if(*p=='\0')    return *s == '\0';
+        if(*(p+1) != '*') {
+            return (*p == *s || (*p=='.' && *s!='\0')) && match(s+1,p+1);
+        }
+        while(*p == *s || (*p=='.' && *s!='\0')) {
+            if(match(s,p+2))    return true;
+            ++s;
+        }
+        return match(s,p+2);
+    }
+    bool isMatch(string s, string p) {
+        return match(s.c_str(), p.c_str());
     }
 };
