abbd2106e085a8307a5f6cbef1aba216e96ae19d
liming-vie <liming.vie@gmail.com>
2016-08-19 13:32:28 +0800

    338. Counting Bits; ret[i] = ret[i/2] + ((i%2) == 1); 2016/08/19

--- CountingBits.cpp
+++ CountingBits.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     vector<int> countBits(int num) {
-        vector<int> ret;
-        ret.reserve(num);
-        int d[4]={0,1,0,1};
-        int cur=0, next=0, idx=0;
-        for(int i=0; i<=num; ++i) {
-            cur += d[next++];
-            ret.push_back(cur);
-            if(next == 4) {
-                cur=ret[++idx];
-                next=0;
+        vector<int> ret(num+1);
+        bool odd=false;
+        for(int i=0; i<=num;++i) {
+            if(odd){
+                ret[i] = ret[i>>1] + 1;
             }
+            else {
+                ret[i] = ret[i>>1];
+            }
+            odd = !odd;
         }
         return ret;
     }
