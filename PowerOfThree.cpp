369490667790d3b0c63526c0b14cbae755e8dfac
liming-vie <liming.vie@gmail.com>
2016-10-12 18:47:33 +0800

    326. Power of Three; Math problem, log3(n) = log10(n) / log10(3); 2016/10/12

---
+++ PowerOfThree.cpp
@@ -0,0 +1,8 @@
+// log3(n) = log10(n) / log10(3)
+
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        return n>0 && ((int)(log10(n) / log10(3)) - (log10(n) / log10(3))) == 0;
+    }
+};
