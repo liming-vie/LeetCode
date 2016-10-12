67d74e59765dfc73c6eb64aad25ea4bed5161f76
liming-vie <liming.vie@gmail.com>
2016-10-12 16:53:40 +0800

    324. Wiggle Sort II; re-arrange the index and use three way partition, make it O(n) time and O(1) space; 2016/10/11

--- WiggleSortII.cpp
+++ WiggleSortII.cpp
@@ -1,38 +1,22 @@
+// refer to  https://discuss.leetcode.com/topic/32929/o-n-o-1-after-median-virtual-indexing
 class Solution {
 public:
-    void mysort(vector<int>& nums, int left, int right, int target) {
-        int l=left, r=right;
-        int mid = l+((r-l)>>1);
-        int tmp = nums[mid];
-        nums[mid] = nums[right];
-        while(l<r) {
-            while(l<r && nums[l]<=tmp)  ++l;
-            if(l<r) nums[r--] = nums[l];
-            while(l<r && nums[r]>tmp)  --r;
-            if(l<r) nums[l++] = nums[r];
-        }
-        nums[l] = tmp;
-        if(l==target) {
-            vector<int> tmp(nums);
-            l=target;
-            r=nums.size()-1;
-            int idx=0;
-            bool odd=false;
-            while(l>=0 || r>target) {
-                if(!odd && l>=0) {
-                    nums[idx++] = tmp[l--];
-                }
-                else if(odd && r>target) {
-                    nums[idx++] = tmp[r--];
-                }
-                odd = !odd;
-            }
-        }
-        else if(l < target) 
-            mysort(nums, l+1, right, target);
-        else    mysort(nums, left, l-1, target);
-    }
     void wiggleSort(vector<int>& nums) {
-        mysort(nums, 0, nums.size() - 1, (nums.size() - 1) >> 1);
+        int n=nums.size();
+        auto midptr = nums.begin() + (n >> 1);
+        nth_element(nums.begin(), midptr, nums.end());
+        int mid = *midptr;
+        // re-arrange the index, when n=10, the new index is 1 3 5 7 9 0 2 4 6 8
+        #define A(i)    (nums[(1+((i)<<1)) % (n|1)])
+        // three way partition
+        int l=0, r=n-1;
+        int i=0;
+        while(i<=r) {
+            if(A(i)>mid) 
+                swap(A(i++),A(l++));
+            else if(A(i)<mid) 
+                swap(A(i),A(r--));
+            else    ++i;
+        }
     }
 };
