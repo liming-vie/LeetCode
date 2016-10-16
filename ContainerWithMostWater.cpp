61fb853c92ac91f2365a198d745bebe93b745bb7
liming-vie <liming.vie@gmail.com>
2016-10-16 15:31:53 +0800

    11. Container With Most Water; 2016/10/16

--- ContainerWithMostWater.cpp
+++ ContainerWithMostWater.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
-    int min(int a,int b){
-        if(a<b) return a;
-        return b;
-    }
-    int maxArea(vector<int> &height) {
-        int l=0,r=height.size()-1;
+    int maxArea(vector<int>& height) {
+        int l=0, r=height.size()-1;
         int res=0;
-        while(l<r){
-            if(res<(r-l)*min(height[l],height[r]))  res=(r-l)*min(height[l],height[r]);
-            if(height[l]<height[r]) ++l;
-            else --r;
+        while(l<r) {
+            int tmp=(r-l)*min(height[l],height[r]);
+            res = res > tmp ? res : tmp;
+            if(height[l] < height[r])   ++l;
+            else    --r;
         }
         return res;
     }
