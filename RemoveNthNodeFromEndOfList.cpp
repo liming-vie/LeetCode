325d38c6fc115fcdd29c31e8bfadbc10ca10c635
liming-vie <liming.vie@gmail.com>
2016-10-22 18:18:05 +0800

    19. Remove Nth Node From End of List; 2016/10/22

--- RemoveNthNodeFromEndOfList.cpp
+++ RemoveNthNodeFromEndOfList.cpp
@@ -8,12 +8,20 @@
  */
 class Solution {
 public:
-    ListNode *removeNthFromEnd(ListNode *head, int n) {
-        ListNode *node1=head,*node2=head,*prev;
-        for(int i=1; i<n; ++i)  node2=node2->next;
-        for(;node2->next; prev=node1,node2=node2->next,node1=node1->next);
-        if(node1==head) return head->next;
-        prev->next=node1->next;
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode *move=head, *pre=NULL, *cur=head;
+        int vi=1;
+        while((vi++)<n)
+            move=move->next;
+        
+        while(move->next) {
+            move=move->next;
+            pre=cur;
+            cur=cur->next;
+        }
+        if(cur==head)   return head->next;
+        
+        pre->next=cur->next;
         return head;
     }
 };
