3c69d5b1d9ee1423538cf207b037df2893d48317
liming-vie <liming.vie@gmail.com>
2016-10-23 15:46:27 +0800

    22. Generate Parentheses; Catalan number; 2016/10/23

--- GenerateParentheses.cpp
+++ GenerateParentheses.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
-
     vector<string> res;
-    
-    void func(int l, int r, string tmp){
-        if(l==0 && r==0){
-            res.push_back(tmp);
+    void func(int l, int r, string cur) {
+        if(l==0 && r==0) res.push_back(cur);
+        else if(l<=r) {
+            if(l>0) func(l-1, r, cur+"(");   
+            if(r>0) func(l, r-1, cur+")");
         }
-        if(l>0){
-            func(l-1,r,tmp+"(");
-        }
-        if(r>0 && r>l)
-            func(l,r-1,tmp+")");
     }
-
     vector<string> generateParenthesis(int n) {
-        func(n,n,"");
-        return res;
+       func(n,n,"");
+       return res;
     }
 };
