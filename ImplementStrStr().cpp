4c793b14adc1f18f220609f723eb125b29814da0
liming-vie <liming.vie@gmail.com>
2016-11-03 17:34:27 +0800

    28. Implement strStr(); KMP; 2016/11/03

--- ImplementStrStr().cpp
+++ ImplementStrStr().cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
-    void getnext(char *s, int *next, int n){
-        int k=-1,i=0;
+    vector<int> next;
+    
+    void func(string pattern) {
+        int len=pattern.length();
+        next.resize(len+1);
         next[0]=-1;
-        while(i<n){
-            while(k>=0 && s[i]!=s[k])   k=next[k];
-            ++i,++k;
-            if(s[i]==s[k])  next[i]=next[k];
-            else next[i]=k;
+        int k=-1,j=0;
+        while(j<len) {
+            if(k<0 || pattern[j]==pattern[k]) {
+                ++j,++k;
+                next[j]=(pattern[j]==pattern[k]?next[k]:k);
+            }
+            else k=next[k];
         }
     }
 
-    int strStr(char *haystack, char *needle) {
-        if(haystack==NULL || needle==NULL)  return-1;
-        
-        int len2=strlen(needle);
-        if(len2==0) return 0;
-        
-        int len1=strlen(haystack);
-        if(len1<len2)   return -1;
-        
-        int *next=new int[len2+1];
-        getnext(needle,next,len2);
-        int i=0,j=0;
-        while(i<len1&&j<len2){
+    int strStr(string haystack, string needle) {
+        func(needle);
+        int l1=haystack.length(), l2=needle.size();
+        int i=0, j=0;
+        while(i<l1 && j<l2) {
             if(j<0||haystack[i]==needle[j]) ++i,++j;
-            else j=next[j];
+            else    j=next[j];
         }
-        if(j==len2) return i-j;
-        return -1;
+        return j==l2 ? i-j : -1;
     }
 };
