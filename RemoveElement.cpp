c7b1387599ad78ecb919a3c787281b4d38959781
liming-vie <liming.vie@gmail.com>
2016-11-03 17:13:18 +0800

    27. Remove Element; 2016/11/03

--- RemoveElement.cpp
+++ RemoveElement.cpp
@@ -1,20 +1,11 @@
 class Solution {
 public:
-    int removeElement(int A[], int n, int elem) {
-        int v[10000],len1=0;
-        int s[10000],len2=0;
-        for(int i=0; i<n; ++i){
-            if(A[i]==elem)
-                v[len1++]=i;
-            else
-                s[len2++]=i;
+    int removeElement(vector<int>& nums, int val) {
+        int n=nums.size(), idx=0;
+        for(int i=0; i<n; ++i) {
+            if(nums[i]!=val) 
+                nums[idx++]=nums[i];
         }
-        if(len1==0 || (len1>0 && v[0] == len2))  return len2;
-        
-        int t=len2-1;
-        for(int i=0; i<len1 && t>=0; ++i,--t){
-            A[v[i]] = A[s[t]];
-        }
-        return len2;
+        return idx;
     }
 };
