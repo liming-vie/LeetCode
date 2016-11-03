606b629f940453103177cbe8e36791b3cd0fe61d
liming-vie <liming.vie@gmail.com>
2016-11-03 20:04:16 +0800

    32. Longest Valid Parentheses; 2016/11/03

--- LongestValidParentheses.cpp
+++ LongestValidParentheses.cpp
@@ -1,22 +1,26 @@
+#define max(a,b)    a<b?b:a
 class Solution {
 public:
-int longestValidParentheses(string s) {
-  int n = s.size();
-  if (n <= 1) return 0;
-  
-  int bp[50000]={0};
-  bp[1]=(s[0]=='('&&s[1]==')')?2:0;
-  int res=bp[1];
-  for(int i=2; i<n; ++i){
-      if(s[i]=='('){
-          bp[i]=0;
-          continue;
-      }
-      if(s[i-1]=='(')  bp[i]=bp[i-2]+2;
-      else if(i>bp[i-1] && s[i-bp[i-1]-1]=='(')   bp[i]=bp[i-1]+2;
-      if(i>=bp[i])    bp[i]+=bp[i-bp[i]];
-      if(res<bp[i])   res=bp[i];
-  }
-  return res;
-}
+    int longestValidParentheses(string s) {
+        int len=s.length();
+        if(len<2)   return 0;
+        
+        // max valid str length ends with i
+        vector<int> dp(len, 0);
+        dp[1]=(s[0]=='(' && s[1]==')')?2:0;
+        
+        int res=dp[1];
+        for(int i=2; i<len; ++i) {
+            if(s[i]=='(')  continue;
+            // two condition
+            if(s[i-1]=='(') dp[i]=dp[i-2]+2;
+            if(i>dp[i-1] && s[i-dp[i-1]-1]=='(')    dp[i]=max(dp[i], dp[i-1]+2);
+            // concate with previews valid string
+            if(i>dp[i])    dp[i]+=dp[i-dp[i]];
+            
+            res=max(res, dp[i]);
+        }
+        
+        return res;
+    }
 };
