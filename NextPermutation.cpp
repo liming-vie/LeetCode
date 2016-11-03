17e02f46f28406bb59f8655b7892c9a07b902348
liming-vie <liming.vie@gmail.com>
2016-11-03 19:44:42 +0800

    31. Next Permutation; 2016/11/03

--- NextPermutation.cpp
+++ NextPermutation.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int len=nums.size();
-        int i=len-2;
-        while(i>=0 && nums[i]>=nums[i+1])
-            --i;
-            
-        if(i<0){
-            reverse(nums.begin(),nums.end());
+        int i=nums.size()-2;
+        // from right to left, find the first one violate increase trend
+        while(i>=0 && nums[i]>=nums[i+1])   --i;
+        
+        // biggest
+        if(i<0) {
+            reverse(nums.begin(), nums.end());
             return;
         }
-        int j=len-1;
-        while(j>i && nums[j]<=nums[i])   --j;
-        
-        int tmp=nums[i];
-        nums[i]=nums[j];
-        nums[j]=tmp;
-        
-        reverse(nums.begin()+i+1,nums.end());
-        return;
+        // from right to left, find the first one bigger than nums[i]
+        int j=nums.size()-1;
+        while(j>i && nums[j]<=nums[i])  --j;
+        //swap the two number
+        swap(nums[i], nums[j]);
+        // reverse the right part of nums[i]
+        reverse(nums.begin()+i+1, nums.end());
     }
 };
