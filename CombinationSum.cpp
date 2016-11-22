967d0ecc04f72d5e594e1efa5bd6640f235f7d52
liming-vie <liming.vie@gmail.com>
2016-11-22 15:25:45 +0800

    39. Combination Sum; 2016/11/22

--- CombinationSum.cpp
+++ CombinationSum.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
-void find(vector<int> &candidates, int vi,int target, vector<int> tmp, int cursum, vector<vector<int> >&res){
-  if (cursum>target)   return;
-
-  int n = candidates.size();
-  for (int i = vi; i<n; ++i){
-    cursum += candidates[i];
-    if (cursum>target) return;
-
-    tmp.push_back(candidates[i]);
-
-    if (cursum == target){
-      res.push_back(tmp);
-      return;
+    vector<vector<int>> res;
+    
+    void func(vector<int>& nums, int vi, vector<int>& cur, int sum, int target) {
+        if(vi == nums.size()) {
+            if(sum==target)
+                res.push_back(cur);
+            return;
+        }
+        // not put this number
+        func(nums, vi+1, cur, sum, target);
+        int s;
+        for(s=sum+nums[vi]; s<=target; s+=nums[vi]) {
+            cur.push_back(nums[vi]);
+            func(nums, vi+1, cur, s, target);
+        }
+        s-=nums[vi];
+        while(s>sum) {
+            s-=nums[vi];
+            cur.pop_back();
+        }
     }
-    find(candidates, i, target, tmp, cursum, res);
-    cursum -= candidates[i];
-    tmp.pop_back();
-  }
-}
 
-vector<vector<int> > combinationSum(vector<int> &candidates, int target) {
-  vector<vector<int> > res;
-  if (target<0)    return res;
-
-  sort(candidates.begin(), candidates.end());
-
-  if (candidates[0]>target)    return res;
-  vector<int> tmp;
-  find(candidates, 0,target, tmp, 0, res);
-  return res;
-}
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> cur;
+        func(candidates, 0, cur, 0, target);
+        return res;
+    }
 };
