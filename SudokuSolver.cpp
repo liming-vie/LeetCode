f8119f6aba697b266ea74dcad140b91da4bc56d7
liming-vie <liming.vie@gmail.com>
2016-11-22 14:22:27 +0800

    37. Sudoku Solver; Use boolean variable to check if valid; 2016/11/22

--- SudokuSolver.cpp
+++ SudokuSolver.cpp
@@ -1,52 +1,37 @@
 class Solution {
 public:
-    bool check(vector<vector<char> >&board,int x,int y){
-        bool col[9],row[9];
-            memset(col,0,sizeof(col));
-            memset(row,0,sizeof(row));
-        for(int i=0; i<9; ++i){
-                if(board[x][i] != '.'){
-                    if(row[board[x][i]-'1'])    return false;
-                    row[board[x][i]-'1']=true;
-                }
-                if(board[i][y]!='.'){
-                    if(col[board[i][y]-'1'])    return false;
-                    col[board[i][y]-'1']=true;
-                }
-        }
-        
-        memset(col,0,sizeof(col));
-        int t1=x-x%3,t2=y-y%3;
-        for(int i=t1;i<t1+3; ++i)
-            for(int j=t2; j<t2+3; ++j){
-                if(board[i][j]!='.'){
-                    if(col[board[i][j]-'1'])    return false;
-                    col[board[i][j]-'1']=true;
-                }
-            }
-        return true;    
-    }
+    bool rows[9][10]={};
+    bool cols[9][10]={};
+    // check each 3*3 array
+    bool little[9][10]={};
     
-    bool fill(int i,int j, vector<vector<char> > &board){
-        if(j==9){
-            i+=1;
-            if(i==9)    return true;
-            j=0;
-        }
-        
-        if(board[i][j]!='.')    return fill(i,j+1,board);
-        
-        for(int k=1; k<10; ++k){
-            board[i][j]='0'+k;
+    bool func(vector<vector<char>>& board, int vi, int vj) {
+        if(vi==9)   return true;
+        if(vj==9)   return func(board, vi+1, 0);
+        if(board[vi][vj] != '.')    return func(board, vi, vj+1);
             
-            if(check(board,i,j) && fill(i,j+1,board))   return true;
-            
-            board[i][j]='.';
+        int t=vi/3*3+vj/3;
+        for(int i=1; i<=9; ++i) {
+            if(rows[vi][i] || cols[vj][i] || little[t][i])
+                continue;
+            rows[vi][i] = cols[vj][i] = little[t][i] = true;
+            board[vi][vj]='0'+i;
+            if (func(board,vi,vj+1))
+                return true;
+            rows[vi][i] = cols[vj][i] = little[t][i] = false;
+            board[vi][vj]='.';
         }
         return false;
     }
-
-    void solveSudoku(vector<vector<char> > &board) {
-        fill(0,0,board);
+    
+    void solveSudoku(vector<vector<char>>& board) {
+        // pre-process 
+        for(int i=0; i<9; ++i)
+            for(int j=0; j<9; ++j) {
+                if(board[i][j] == '.')  continue;
+                int t=board[i][j]-'0';
+                rows[i][t]=cols[j][t]=little[i/3*3+j/3][t]=true;
+            }
+        func(board,0,0);
     }
 };
