523882afe096c2b2bffa38c2b303a79bc46012ca
liming-vie <liming.vie@gmail.com>
2016-11-22 15:39:38 +0800

    40. Combination Sum II; Use a boolean array to check if use; 2016/11/22

--- CombinationSumII.cpp
+++ CombinationSumII.cpp
@@ -1,41 +1,31 @@
 class Solution {
 public:
-    void find(vector<int>&num, int vi, int target, vector<int>tmp, int cursum, set<vector<int> >&res){
-        if(vi==num.size()||cursum>target)   return;
-        
-        int n=num.size();
-        for(int i=vi; i<n;++i){
-            cursum+=num[i];
-            if(cursum>target)   return;
-            
-            tmp.push_back(num[i]);
-            
-            if(cursum==target){
-                res.insert(tmp);
-                return;
-            }
-            
-            find(num,i+1,target,tmp,cursum,res);
-            
-            cursum-=num[i];
-            tmp.pop_back();
+    vector<vector<int>> res;
+    vector<bool> use;
+    
+    void func(vector<int>& nums, int vi, vector<int>& cur, int sum, int target) {
+        if(sum > target)    return;
+        if(vi == nums.size()) {
+            if(sum==target)
+                res.push_back(cur);
+            return;
         }
+        // not put this number
+        func(nums, vi+1, cur, sum, target);
+        // only when thr previous one being used, the same one can be use
+        if(vi && nums[vi] == nums[vi-1] && !use[vi-1])  return;
+        // put once
+        use[vi]=true;
+        cur.push_back(nums[vi]);
+        func(nums, vi+1, cur, sum+nums[vi], target);
+        cur.pop_back();
+        use[vi]=false;
     }
-
-    vector<vector<int> > combinationSum2(vector<int> &num, int target) {
-        vector<vector<int> >res;
-        if(num.size()==0||target<0)   return res;
-        
-        sort(num.begin(),num.end());
- 
-        if(num[0]>target)   return res;
-        
-        vector<int> tmp;
-        set<vector<int> > tres;
-        find(num,0,target,tmp, 0,tres);
-        for(set<vector<int> >::iterator i=tres.begin(); i!=tres.end(); ++i){
-            res.push_back(*i);
-        }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        use.resize(candidates.size(), false);
+        vector<int> cur;
+        func(candidates, 0, cur, 0, target);
         return res;
     }
 };
