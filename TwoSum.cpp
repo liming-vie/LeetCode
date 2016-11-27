818e2d0ac971d7d34e03b44de313876e498f1d21
liming-vie <liming.vie@gmail.com>
2016-11-27 22:42:55 +0800

    1. Two Sum; Use hash table to achieve O(n) time but with O(n) space; 2016/11/27

--- TwoSum.cpp
+++ TwoSum.cpp
@@ -1,32 +1,17 @@
 class Solution {
 public:
-    struct Num {
-        int val;
-        int idx;
-    };
-    static bool comp(Num &a,Num &b){
-      return a.val < b.val;
-    }
     vector<int> twoSum(vector<int>& nums, int target) {
-        int size=nums.size();
-        if(size <= 1)   return vector<int>();
+        multimap<int,int> mp;
+        for(int i=0; i<nums.size(); ++i)
+            mp.insert(make_pair(nums[i],i));
         
-        Num *data = new Num[size];
-        for(int i=0; i<size; ++i) {
-            data[i] = {nums[i], i};
+        for(auto p=mp.begin(); p!=mp.end(); ++p) {
+            auto t=mp.find(target-p->first);
+            if(t!=mp.end()) {
+                if(t==p) ++t;
+                return vector<int>{p->second, t->second};
+            }
         }
-        sort(data, data + size, comp);
-        
-        int l=0, r=size-1;
-        int tmp;
-        while(l<r) {
-            tmp=data[l].val + data[r].val;
-            if(tmp < target) ++l;
-            else if(tmp > target) --r;
-            else break;
-        }
-        int idx1 = data[l].idx;
-        int idx2 = data[r].idx;
-        return idx1 < idx2 ? vector<int>{idx1, idx2} : vector<int>{idx2, idx1};       
+        return vector<int>();
     }
 };
