7f4a127e450540c134944f4a882d317fdb9cb862
liming-vie <liming.vie@gmail.com>
2016-11-29 14:08:10 +0800

    47. Permutations II; use next permutation; 2016/11/29

--- PermutationsII.cpp
+++ PermutationsII.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
-    vector<vector<int>> res;
-    
-    void func(vector<int>&nums, int vi) {
-        if(vi==nums.size()) {
-            res.push_back(nums);
-            return;
-        }
-            
-        for(int i=vi; i<nums.size(); ++i) {
-            // nums[vi,i) != nums[i]
-            bool flag=true;
-            for(int j=vi; j<i; ++j) {
-                if(nums[j] == nums[i]) {
-                    flag=false;
-                    break;
-                }
-            }
-            if(!flag)   continue;
-            swap(nums[i], nums[vi]);
-            func(nums, vi+1);
-            swap(nums[i], nums[vi]);
-        }
+    // use next permutation
+    bool next(vector<int>& nums) {
+        int i=nums.size()-1;
+        while(i && nums[i]<=nums[i-1])   --i;
+        if(i==0)    return false;
+        
+        --i;
+        int j=nums.size()-1;
+        while(j>i && nums[j]<=nums[i])  --j;
+        
+        swap(nums[i], nums[j]);
+        
+        reverse(nums.begin()+i+1, nums.end());
+        return true;
     }
 
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        func(nums, 0);
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+        res.push_back(nums);
+        while(next(nums)) {
+            res.push_back(nums);
+        }
         return res;
     }
 };
