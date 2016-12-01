e221eb65c954fd2f1975a8e9955f35aa000e022b
liming-vie <liming.vie@gmail.com>
2016-12-01 13:50:23 +0800

    60. Permutation Sequence; 2016/12/01

--- PermutationSequence.cpp
+++ PermutationSequence.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
-    int t[11]={1,1};
-    bool is[11]={};
-    
-    int findk(int k, int n){
-        int c=0;
-        for(int i=1; i<=n; ++i){
-            if(is[i])   continue;
-            if((++c) == k){
-                is[i]=true;
-                return i;
-            }    
-        }
-    }
-    
     string getPermutation(int n, int k) {
-        for(int i=2; i<=n; ++i){
-            t[i]=t[i-1]*i;
-        }
+        int fac[11]={1,1};
+        for(int i=1; i<=n; ++i) fac[i] = fac[i-1]*i;
         
+        --k;
         string res="";
-        
-        for(int i=0; i<n; ++i){
-            res+=(findk((k-1)/t[n-i-1] + 1, n)+'0');
-            k%=t[n-i-1];
-            if(k==0)    k=t[n-i-1];
+        bool use[11]={};
+        for(int i=n-1; i>=0; --i) {
+            int t=k/fac[i];
+            // find t-th number
+            int cur=0;
+            for(int j=1; j<=n; ++j) {
+                if(use[j])  continue;
+                if(cur==t) {
+                    cur=j;
+                    break;
+                }
+                ++cur;
+            }
+            use[cur]=true;
+            res += (char)(cur+'0');
+            k%=fac[i];
         }
         return res;
     }
