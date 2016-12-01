a996d92b37c19fc95229260a1946bb3a42d2b02f
liming-vie <liming.vie@gmail.com>
2016-12-01 15:01:10 +0800

    63. Unique Paths II; one dimensional dp; 2016/12/01

--- UniquePathsII.cpp
+++ UniquePathsII.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int m=obstacleGrid.size();
-        if(m==0)    return 0;
-        int n=obstacleGrid[0].size();
-        int dp[105][105]={};
-        for(int j=n-1; j>=0 && !obstacleGrid[m-1][j]; --j)
-            dp[m-1][j]=1;
-        for(int i=m-2; i>=0; --i)
-            for(int j=n-1; j>=0; --j){
-                if(obstacleGrid[i][j])  continue;
-                int a=dp[i+1][j];
-                int b=(j==n-1?0:dp[i][j+1]);
-                dp[i][j]=a+b;
+        int n=obstacleGrid.size(), m=n==0?0:obstacleGrid[0].size();
+        if(!n || !m || obstacleGrid[0][0]==1)   
+            return 0;
+            
+        vector<int> dp(m+1,0);
+        dp[1]=1;
+        for(int i=1; i<=n; ++i) {
+            for(int j=1; j<=m; ++j) {
+                if(obstacleGrid[i-1][j-1]) 
+                    dp[j]=0;
+                else {
+                    dp[j]+=dp[j-1];
+                }
             }
-        return dp[0][0];
+        }
+        return dp[m];
     }
 };
