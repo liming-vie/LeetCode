697c8830374deff9243aab5b537dd3f2fa7e3e70
liming-vie <liming.vie@gmail.com>
2016-12-01 23:27:14 +0800

    66. Plus One; 2016/12/01

--- PlusOne.cpp
+++ PlusOne.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        int n=digits.size();
-        digits[n-1]++;
-        int y=0;
-        for(int i=n-1; i>=0; --i){
-            digits[i]+=y;
+        int y=1;
+        for(int i=digits.size()-1; i>=0; --i) {
+            digits[i] += y;
             y=digits[i]/10;
             digits[i]%=10;
         }
-        if(y>0) digits.insert(digits.begin(),y);
+        if(y) {
+            digits.insert(digits.begin(), y);
+        }
         return digits;
     }
 };
