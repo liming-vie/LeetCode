33311428500e857c0e80b5a83da04fb7c6dbfee3
liming-vie <liming.vie@gmail.com>
2016-12-03 17:18:28 +0800

    68. Text Justification; 2016/12/03

--- TextJustification.cpp
+++ TextJustification.cpp
@@ -1,45 +1,49 @@
 class Solution {
 public:
+    void fill(stringstream&ss, int size) {
+        for(int i=0; i<size; ++i)
+            ss<<' ';
+    }
     vector<string> fullJustify(vector<string>& words, int maxWidth) {
         vector<string> res;
         int n=words.size();
-        if(!n)  return res;
-        int curl=0;
-        int curn=0;
-        int start=0;
-        for(int i=0; i<n; ++i){
-            if(curl+words[i].length()+((curl>0)?1:0)<=maxWidth){
-                curl+=words[i].length()+(curl>0?1:0);
-                curn++;
-                continue;
-            }
-            string line=words[start];
-            if(curn==1){
-                for(int j=line.length(); j<maxWidth; ++j)
-                    line+=' ';
-            }else{
-                int space=maxWidth-curl+curn-1;
-                int k=space%(curn-1);
-                int t=space/(curn-1);
-                for(int i=1; i<curn; ++i){
-                    if(k>=i)    line+=' ';
-                    for(int j=0; j<t; ++j)  line+=' ';
-                    line+=words[start+i];
+        if(n==0)    return res;
+        
+        int curl=words[0].length(), left=0;
+        stringstream ss;
+        
+        for(int i=1; i<n; ++i) {
+            if(curl+1+words[i].length()>maxWidth) {
+                // fill words ans blanks
+                int blank_count=i-left-1;
+                int tot_blank=(maxWidth-curl)+blank_count;
+                ss<<words[left];
+                if(i-1==left) {
+                    fill(ss,tot_blank);
                 }
+                else {
+                    int blank_size=tot_blank/blank_count;
+                    int mod=tot_blank%blank_count;
+                    for(int j=left+1; j<i; ++j) {
+                        if(j<=left+mod) fill(ss, blank_size+1);
+                        else    fill(ss, blank_size);
+                        ss<<words[j];
+                    }
+                }
+                res.push_back(ss.str());
+                ss.str("");
+                left=i;
+                curl=words[i].length();
+            } else {
+                curl+=1+words[i].length();
             }
-            res.push_back(line);
-            curn=1;
-            curl=words[i].length();
-            start=i;
         }
-        string line=words[start];
-        for(int i=1; i<curn; ++i){
-            line+=' ';
-            line+=words[start+i];
+        ss<<words[left];
+        for(int i=left+1; i<n; ++i) {
+            ss<<' '<<words[i];
         }
-        curl=maxWidth-curl;
-        for(int i=0; i<curl; ++i)   line+=' ';
-        res.push_back(line);
+        fill(ss, maxWidth-ss.str().length());
+        res.push_back(ss.str());
         return res;
     }
 };
