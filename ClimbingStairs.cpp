3a70b0d969aa32a84dfdbda462513bb322ec20e2
liming-vie <liming.vie@gmail.com>
2016-12-03 19:54:09 +0800

    70. Climbing Stairs; 2016/12/03

--- ClimbingStairs.cpp
+++ ClimbingStairs.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n<=1)    return 1;
-        int *count=new int[n+1];
-        for(int i=1; i<=n; ++i) count[i]=0;
-        
-        count[0]=1;
-        count[1]=1;
-        for(int i=2; i<=n; ++i){
-            count[i]+=count[i-1]+count[i-2];
+        vector<int> dp(n+1);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2; i<=n; ++i) {
+            dp[i]=dp[i-1]+dp[i-2];
         }
-        return count[n];
+        return dp[n];
     }
 };
