ee56ba76f2abc527daf04f928a2e3e88f4617c71
liming-vie <liming.vie@gmail.com>
2016-12-03 19:50:46 +0800

    69. Sqrt(x); use unsigned long long to avoid overflow; 2016/12/03

--- Sqrt(x).cpp
+++ Sqrt(x).cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if(x<=1ull) return x;
-        unsigned long long l=0,r=(x+1)/2;
-        unsigned long long mid;
-        unsigned long long res;
-        while(l<r){
+        int l=0, r=x;
+        unsigned long long mid,tmp;
+        while(l<=r) {
             mid=(l+r)>>1;
-            res=mid*mid;
-            if(res==x)  return mid;
-            if(res<x)   l=mid+1;
-            else    r=mid-1;
+            tmp=mid*mid;
+            if(tmp < x)
+                l=mid+1;
+            else if(tmp>x) 
+                r=mid-1;
+            else
+                return mid;
         }
-        res=r*r;
-        if(res>x)   return r-1;
         return r;
     }
 };
