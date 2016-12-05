1f7d8da5d45f83a171c0fecdd59c36db2ed97d3b
liming-vie <liming.vie@gmail.com>
2016-12-05 15:40:23 +0800

    74. Search a 2D Matrix; use macro define to simplify code; 2016/12/05

--- SearchA2DMatrix.cpp
+++ SearchA2DMatrix.cpp
@@ -1,31 +1,24 @@
+#define BINARY(val)                  \
+    while(l<r) {                     \
+        mid=(l+r)>>1;                \
+        if(val<target)   l=mid+1;    \
+        else if(val>target)  r=mid-1;\
+        else return true;            \
+    }
+
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int u=0, d=matrix.size()-1;
-        int l=0, r=(d<0?-1:matrix[0].size()-1);
-        if(d<0||r<0)    return false;
+        int l=0, r=matrix.size()-1, mid;
+        BINARY(matrix[mid][0]);
         
-        int mid;
-        while(u<d){
-            mid=(u+d)>>1;
-            if(matrix[mid][0]==target)    return true;
-            if(matrix[mid][0] < target)   u=mid+1;
-            else    d=mid-1;
-        }
+        int row=l;
+        if(matrix[l][0]>target) --row;
+        if(row<0)   return false;
         
-        int vi=u;
-        if(matrix[u][0]>target)    vi=u-1;
+        l=0, r=matrix[0].size()-1;
+        BINARY(matrix[row][mid]);
         
-        if(vi<0)    return false;
-        
-        while(l<r){
-            mid=(l+r)>>1;
-            if(matrix[vi][mid]==target) return true;
-            if(matrix[vi][mid]<target)    l=mid+1;
-            else    r=mid-1;
-        }
-        
-        if(matrix[vi][l]==target)   return true;
-        return false;
+        return matrix[row][l]==target;
     }
 };
