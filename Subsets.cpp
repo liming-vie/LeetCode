10fb7d026408680aef261d1e202d732e2345ea08
liming-vie <liming.vie@gmail.com>
2016-12-08 10:03:46 +0800

    78. Subsets; bit manipulation; 2016/12/07

--- Subsets.cpp
+++ Subsets.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
-    vector<int> cur;
-    vector<vector<int>> res;
-    
-    void func(vector<int>& nums, int vi) {
-        if(vi==nums.size()) {
-            res.push_back(cur);
-            return;
-        }
-        
-        cur.push_back(nums[vi]);
-        func(nums, vi+1);
-        cur.pop_back();
-        
-        func(nums, vi+1);
-    }
-
     vector<vector<int>> subsets(vector<int>& nums) {
-        func(nums, 0);
+        vector<vector<int>> res;
+        vector<int> cur;
+        int size=1<<nums.size();
+        for(int k=0; k<size; ++k) {
+            cur.clear();
+            for(int i=0; i<nums.size(); ++i) 
+                if((1<<i) & k)
+                    cur.push_back(nums[i]);
+            res.push_back(cur);
+        }
         return res;
     }
 };
