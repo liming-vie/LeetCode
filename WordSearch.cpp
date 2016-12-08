bb3d3b8a4cc3da96b56b664af5d19e9df7c67dc1
liming-vie <liming.vie@gmail.com>
2016-12-08 13:06:01 +0800

    79. Word Search; 2016/12/07

--- WordSearch.cpp
+++ WordSearch.cpp
@@ -1,36 +1,35 @@
 class Solution {
 public:
-    bool is[500][500]={};
-    int m,n,len;
-    bool exist(vector<vector<char>>& board, string word) {
-        m=board.size();
-        if(m!=0)    n=board[0].size();
-        len=word.length();
-        for(int i=0; i<m; ++i)
-            for(int j=0; j<n; ++j){
-                if(func(i,j,0,board,word)) 
-                    return true;
-            }
-        return false;
-    }
+    int m, n;
+    vector<vector<bool>> use;
+    int d[4][2]={{1,0},{-1,0},{0,1},{0,-1}};
     
-    bool func(int vi,int vj, int index, vector<vector<char>>& board, string word){
-        if(index==len)    return true;
+    bool search(vector<vector<char>>&board, string& word, int vi, int vj, int len) {
+        if(len==word.length())  return true;
         
-        if(vi<0||vj<0||vi==m||vj==n||is[vi][vj]||board[vi][vj]!=word[index])  return false;
+        if (vi<0 || vj<0 || vi==m || vj==n || use[vi][vj] || board[vi][vj]!=word[len])  return false;
         
-        is[vi][vj]=true;
-        
-        int d[4][2]={{-1,0},{1,0},{0,-1},{0,1}};
-        for(int i=0; i<4; ++i){
-            int x=vi+d[i][0],y=vj+d[i][1];
-            
-            if(func(x,y,index+1,board,word))
+        use[vi][vj]=true;
+        for(int i=0; i<4; ++i) {
+            if(search(board, word, vi+d[i][0], vj+d[i][1], len+1))
                 return true;
         }
+        use[vi][vj]=false;
+        return false;
+    }
+
+    bool exist(vector<vector<char>>& board, string word) {
+        m=board.size();
+        n=board.size()==0?0:board[0].size();
+        if(!m || !n)    return false;
         
-        is[vi][vj]=false;
-        
+        use=vector<vector<bool>>(m, vector<bool>(n));
+        for(int i=0; i<m; ++i) {
+            for(int j=0; j<n; ++j) {
+                if(word[0]==board[i][j] && search(board, word, i, j, 0)) 
+                    return true;
+            }
+        }
         return false;
     }
 };
