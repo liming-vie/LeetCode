4e8bda0020461c05dfc39f5663ddb971d43165a3
liming-vie <liming.vie@gmail.com>
2016-12-15 21:33:23 +0800

    89. Gray Code; Another way; 2016/12/15

--- GrayCode.cpp
+++ GrayCode.cpp
@@ -2,13 +2,9 @@ class Solution {
 public:
     vector<int> grayCode(int n) {
         int size=1<<n;
-        vector<int> res(size, 0);
-        int idx=1;
-        for(int i=0; i<n; ++i) {
-            int k=1<<i;
-            for(int j=idx-1; j>=0; --j) {
-                res[idx++]=res[j]|k;
-            }
+        vector<int> res(size);
+        for(int i=0; i<size; ++i) {
+            res[i]=i^(i>>1);
         }
         return res;
     }
