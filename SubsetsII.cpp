4c47e31d1d267728d13cb13064057fccd7580f69
liming-vie <liming.vie@gmail.com>
2016-12-15 21:46:12 +0800

    90. Subsets II; no need recorde whole use array; 2016/12/15

--- SubsetsII.cpp
+++ SubsetsII.cpp
@@ -2,27 +2,25 @@ class Solution {
 public:
     vector<vector<int>> res;
     vector<int> cur;
-    vector<bool> use;
 
-    void func(vector<int>&nums, int vi) {
+    void func(vector<int>&nums, int vi, bool lastuse) {
         if(vi==nums.size()) {
             res.push_back(cur);
             return ;
         }
-        func(nums, vi+1);
-        if(vi==0 || nums[vi]!=nums[vi-1] || use[vi-1]) {
-            cur.push_back(nums[vi]);
-            use[vi]=true;
-            func(nums, vi+1);
-            cur.pop_back();
-            use[vi]=false;
-        }
+        func(nums, vi+1, false);
+        
+        if(vi && nums[vi]==nums[vi-1] && !lastuse)
+            return;
+            
+        cur.push_back(nums[vi]);
+        func(nums, vi+1, true);
+        cur.pop_back();
     }
 
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        use.resize(nums.size(), false);
         sort(nums.begin(), nums.end());
-        func(nums, 0);
+        func(nums, 0, false);
         return res;
     }
 };
