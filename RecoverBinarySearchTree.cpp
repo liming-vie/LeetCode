1651deede0213ca9832964824c2a7e06496d6a05
liming-vie <liming.vie@gmail.com>
2016-12-17 17:54:14 +0800

    99. Recover Binary Search Tree; Use Morris Traversal; O(1) space; 2016/12/17

--- RecoverBinarySearchTree.cpp
+++ RecoverBinarySearchTree.cpp
@@ -7,27 +7,37 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+// morris traversal; refer to http://www.cnblogs.com/grandyang/p/4298069.html
+ 
+ #define CHECK()    \
+    if(last && last->val > cur->val) {  \
+        if(!first)  first=last;         \
+        second=cur;                     \
+    }                                   \
+    last=cur;
+ 
 class Solution {
 public:
     void recoverTree(TreeNode* root) {
-        stack<TreeNode*> st;
-        TreeNode *move=root, *pre=NULL, *first=NULL, *second=NULL;
-        while(!st.empty() || move) {
-            if(move) {
-                st.push(move);
-                move=move->left;
-            } else {
-                move=st.top();
-                st.pop();
-                if(pre && pre->val>move->val) {
-                    if(!first)   first=pre;
-                    second=move;
+        TreeNode *pre, *cur=root, *first=NULL, *second=NULL, *last=NULL;
+        while(cur) {
+            if(cur->left) {
+                pre=cur->left;
+                while(pre->right && pre->right != cur) pre=pre->right;
+                if(pre->right) {
+                    CHECK();
+                    cur=cur->right;
+                    pre->right=NULL;
+                } else {
+                    pre->right=cur;
+                    cur=cur->left;
                 }
-                pre=move;
-                move=move->right;
+            } else {
+                CHECK();
+                cur=cur->right;
             }
         }
-        if(first && second) 
-            swap(first->val, second->val);
+        if(first && second) swap(first->val, second->val);
     }
 };
