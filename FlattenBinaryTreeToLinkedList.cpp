c7a30168951d5894ccd9b894cb2a2955f345d3fe
liming-vie <liming.vie@gmail.com>
2016-12-18 17:22:28 +0800

    114. Flatten Binary Tree to Linked List; use left child to record origin right child; 2016/12/18

---
+++ FlattenBinaryTreeToLinkedList.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        if(!root)   return;
+        
+        stack<TreeNode*> st;
+        TreeNode*move=root;
+        TreeNode*pre=NULL;
+        while(!st.empty() || move) {
+            if(move) {
+                st.push(move);
+                if(pre) pre->right=move;
+                pre=move;
+                move=move->left;
+                pre->left=pre->right;
+            } else {
+                move=st.top();
+                st.pop();
+                auto tmp=move;
+                move=move->left;
+                tmp->left=NULL;
+            }
+        }
+    }
+};
