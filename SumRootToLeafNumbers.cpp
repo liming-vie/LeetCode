113d328fe33aef1d4846e67d117669082eaa64d4
liming-vie <liming.vie@gmail.com>
2016-12-19 17:22:48 +0800

    129. Sum Root to Leaf Numbers; iterative; 2016/12/19

--- SumRootToLeafNumbers.cpp
+++ SumRootToLeafNumbers.cpp
@@ -7,24 +7,25 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+#define MP(a,b) make_pair((a),(b))
 class Solution {
 public:
-    int res=0;
-
-    void func(TreeNode* root, int num) {
-        if(!root)   return;
-        
-        num=num*10+root->val;
-        if(!root->left && !root->right) {
-            res+=num;
-            return;
-        }
-        func(root->left, num);
-        func(root->right, num);
-    }
-
     int sumNumbers(TreeNode* root) {
-        func(root, 0);
+        if(!root)   return 0;
+        queue<pair<TreeNode*, int>> q;
+        q.push(MP(root, root->val));
+        int res=0;
+        while(!q.empty()) {
+            auto t=q.front(); q.pop();
+            int num=t.second;
+            if(!t.first->left && !t.first->right)
+                res+=num;
+            else {
+                num*=10;
+                if(t.first->left)   q.push(MP(t.first->left, num+t.first->left->val));
+                if(t.first->right)   q.push(MP(t.first->right, num+t.first->right->val));
+            }
+        }
         return res;
     }
 };
