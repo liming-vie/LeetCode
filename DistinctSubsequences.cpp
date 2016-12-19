c2b8366c22936fcf56a2d430b26a9bfeff4ad4a9
liming-vie <liming.vie@gmail.com>
2016-12-19 14:20:48 +0800

    115. Distinct Subsequences; O(n^2); 2016/12/19

--- DistinctSubsequences.cpp
+++ DistinctSubsequences.cpp
@@ -2,22 +2,14 @@ class Solution {
 public:
     int numDistinct(string s, string t) {
         int sl=s.length(), tl=t.length();
-        vector<int> dp(sl+1,0);
+        vector<int> dp(tl+1,0);
         dp[0]=1;
-        for(int i=1; i<=tl; ++i) {
-            for(int j=sl; j>=1; --j) {
-                dp[j]=0;
-                if(t[i-1]!=s[j-1])  continue;
-                
-                for(int k=j-1; k>=0; --k) {
-                    dp[j]+=dp[k];
-                }
+        for(int i=1; i<=sl; ++i) {
+            for(int j=tl; j>=0; --j) {
+                if(s[i-1]==t[j-1]) 
+                    dp[j]+=dp[j-1];
             }
-            dp[0]=0;
         }
-        int res=0;
-        for(int i=1; i<=sl; ++i)
-            res+=dp[i];
-        return res;
+        return dp[tl];
     }
 };
