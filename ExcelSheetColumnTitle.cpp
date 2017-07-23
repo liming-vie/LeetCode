ae8d43f40c4c776246e068326dd7a99022b9a3a5
liming-vie <liming.vie@gmail.com>
2017-07-22 18:25:55 -0700

    168. Excel Sheet Column Title; 2017/07/22

---
+++ ExcelSheetColumnTitle.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string convertToTitle(int n) {
+        string res = "";
+        while (n) {
+            res = (char)(((n-1)%26) + (int)'A')+res;
+            n=(n-1)/26;
+        }
+        
+        return res;
+    }
+};
