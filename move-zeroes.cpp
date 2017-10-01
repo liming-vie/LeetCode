1e269a59a4038dcf11a50883daa89044467f51c2
liming-vie <liming.vie@gmail.com>
2017-10-02 00:27:09 +0800

    283. Move Zeroes; 2017/10/02

---
+++ move-zeroes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i=0, j=0;
+        int n=nums.size();
+        while(i<n) {
+            if (nums[i]!=0) {
+                nums[j++]=nums[i];
+            }
+            ++i;
+        }
+        while(j<n) {
+            nums[j++]=0;
+        }
+    }
+};
