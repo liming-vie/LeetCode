9131e56450cfd53ed012548a729a4c2bf0237bc5
liming-vie <liming.vie@gmail.com>
2017-10-05 01:13:16 +0800

    91. Decode Ways; dp; 2017/10/05

--- decode-ways.cpp
+++ decode-ways.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     int numDecodings(string s) {
-        int len=s.length();
-        if(!len)    return 0;
+        int l=s.length();
+        if (l==0 || s[0] == '0')   return 0;
         
-        vector<int> dp(len+1);
-        dp[0]=1;
-        for(int i=1; i<=len; ++i) {
-            if(s[i]=='0')   dp[i]=dp[i-1];
+        vector<int> dp(l+1, 1);
+        for(int i=1; i<l; ++i) {
+            if(s[i-1] == '1' || (s[i-1] == '2' && s[i] < '7')) {
+                dp[i+1] = dp[i-1];
+                if (s[i] != '0') {
+                    dp[i+1] += dp[i];
+                }
+            } else if (s[i] == '0') {
+                return 0;
+            }
             else {
-                dp[i]=dp[i-1];
-                if(i>1 && (s[i-2]=='1' || (s[i-2]=='2' && s[i-1]<'7')))
-                    dp[i]+=dp[i-2];
+                dp[i+1] = dp[i];
             }
         }
-        return dp[len];
+        return dp[l];
     }
 };
