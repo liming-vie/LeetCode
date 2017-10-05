ff0f0e3da1ec5e35859ed7bc28d95ecde537863c
liming-vie <liming.vie@gmail.com>
2017-10-06 00:57:37 +0800

    10. Regular Expression Matching; dp; 2017/10/06

--- regular-expression-matching.cpp
+++ regular-expression-matching.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
-    inline bool isMatch(string s, string p) {
-        int l1=s.length(),l2=p.length();
-        vector<vector<bool>>dp(l1+1,vector<bool>(l2+1,false));
-        dp[0][0]=true;
-        for(int i=1; i<=l1; ++i){
-            dp[i][0] = false;
+    bool isMatch(string s, string p) {
+        int sl=s.length(), pl=p.length();
+        vector<vector<bool>> match(sl+1, vector<bool>(pl+1, false));
+        match[0][0] = true;
+        for (int i=1; i<=pl; ++i) {
+            match[0][i] = (p[i-1] == '*' && match[0][i-2]); // match begin
         }
-        for(int j=1; j<=l2; ++j) {
-            dp[0][j] = p[j-1]=='*' ? dp[0][j-2] : false;
-        }
-        for(int i=1; i<=l1; ++i) {
-            for(int j=1; j<=l2; ++j) {
-                if(p[j-1] == '*') {
-                    dp[i][j] = dp[i][j-1] || dp[i][j-2] || (dp[i-1][j] && (p[j-2]=='.'||p[j-2]==s[i-1]));
-                }
-                else {
-                    dp[i][j] = dp[i-1][j-1] && (p[j-1]=='.' || p[j-1] == s[i-1]);
+        
+        for (int i=1; i<=sl; ++i) {
+            for (int j=1; j<=pl; ++j) {
+                switch (p[j-1]) {
+                    case '*':
+                        match[i][j] = match[i][j-2] // zero
+                            || match[i][j-1] // one
+                            || (match[i-1][j] && (p[j-2] == '.' || p[j-2] == s[i-1])); // multiple
+                        break;
+                    case '.':
+                        match[i][j] = match[i-1][j-1];
+                        break;
+                    default:
+                        match[i][j] = (p[j-1] == s[i-1]) && match[i-1][j-1];
                 }
             }
         }
-        return dp[l1][l2];
+        
+        return match[sl][pl];
     }
 };
