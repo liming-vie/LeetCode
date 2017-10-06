14e4b3373691d1e15c4c59c564fb9a33fc2e7f1a
liming-vie <liming.vie@gmail.com>
2017-10-06 23:43:31 +0800

    121. Best Time to Buy and Sell Stock; 2017/10/06

--- best-time-to-buy-and-sell-stock.cpp
+++ best-time-to-buy-and-sell-stock.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        if(n<2) return 0;
-        int min=prices[0], res=0;
-        for(int i=1; i<n; ++i) {
-            min=min<prices[i]?min:prices[i];
-            int tmp=prices[i]-min;
-            res=tmp>res?tmp:res;
+        if (n<2)    return 0;
+        int min = prices[0], res=0;
+        for (int i=1; i<n; ++i) {
+            int tmp = prices[i] - min;
+            res = tmp < res ? res : tmp;
+            min = min < prices[i] ? min : prices[i];
         }
         return res;
     }
