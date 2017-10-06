ebefb59f89ab7d1856d4d9c0fcafbead50d904b2
liming-vie <liming.vie@gmail.com>
2017-10-06 22:19:43 +0800

    76. Minimum Window Substring; O(n); 2017/10/06

--- minimum-window-substring.cpp
+++ minimum-window-substring.cpp
@@ -1,35 +1,45 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int need[150]={0};
-        for(auto c : t)
+        if (s.length() < t.length())    return "";
+        
+        unordered_map<char, int> need;
+        for(auto c:t) {
             ++need[c];
+        }
+        
+        int resl = -1, ress = -1;
+        int start=0, tot=0;
+        unordered_map<char, int> count;
+        for (int i=0; i<s.length(); ++i) {
+            if (need.find(s[i]) == need.end()) {
+                continue;
+            }
             
-        int slen=s.length(), tlen=t.length();
-        int count[150]={0};
-        int tot=0, begin=0;
-        int resl=slen+1, resb;
-        for(int i=0; i<slen; ++i) {
-            if(need[s[i]]==0)   continue;
+            if (++count[s[i]] <= need[s[i]]) ++tot;
             
-            if(++count[s[i]] <= need[s[i]]) ++tot;
-            
-            if(tot>=tlen) {
-                for(; begin<=i; ++begin) {
-                    if(need[s[begin]]==0)   continue;
-                    if(count[s[begin]] > need[s[begin]]) --count[s[begin]];
-                    else    break;
+            if (tot >= t.length()) {
+                for (; start<i; ++start) {
+                    if (need.find(s[start]) == need.end()) {
+                        continue;
+                    }
+                    else if (count[s[start]] > need[s[start]]) {
+                        --count[s[start]];
+                    }
+                    else {
+                        break;
+                    }
                 }
                 
-                int tmp=i-begin+1;
-                if(tmp<resl) {
-                    resl=tmp;
-                    resb=begin;
+                int tmp = i - start + 1;
+                if (resl == -1 || resl > tmp) {
+                    resl = tmp;
+                    ress = start;
                 }
             }
         }
         
-        if(resl==slen+1)    return "";
-        return s.substr(resb, resl);
+        if (resl == -1) return "";
+        return s.substr(ress, resl);
     }
 };
