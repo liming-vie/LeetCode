26b92a2e97e9ea1d61c79e8926985f7f1028ede8
liming-vie <liming.vie@gmail.com>
2017-10-07 00:16:23 +0800

    1. Two Sum; hash; 2017/10/07

--- two-sum.cpp
+++ two-sum.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        multimap<int,int> mp;
-        for(int i=0; i<nums.size(); ++i)
-            mp.insert(make_pair(nums[i],i));
-        
-        for(auto p=mp.begin(); p!=mp.end(); ++p) {
-            auto t=mp.find(target-p->first);
-            if(t!=mp.end()) {
-                if(t==p) ++t;
-                return vector<int>{p->second, t->second};
+        unordered_map<int, int> mp;
+        for (int i=0; i<nums.size(); ++i) {
+            if (mp.find(target-nums[i]) != mp.end()) {
+                return vector<int>({mp[target-nums[i]], i});
             }
+            mp.insert(make_pair(nums[i], i));
         }
+        
         return vector<int>();
     }
 };
