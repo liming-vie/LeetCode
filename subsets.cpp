79fc8e5d659ea5c1a52b89a66e88af04809cfbfd
liming-vie <liming.vie@gmail.com>
2017-10-07 20:59:23 +0800

    78. Subsets; bit manipulation; 2017/10/07

--- subsets.cpp
+++ subsets.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
-    vector<vector<int>> res;
-    vector<int> cur;
-    
-    void dfs(const vector<int>& nums, int vi) {
-        if (vi == nums.size()) {
-            res.push_back(cur);
-            return ;
-        }
-        
-        dfs(nums, vi+1);
-        
-        cur.push_back(nums[vi]);
-        dfs(nums, vi+1);
-        cur.pop_back();
-    }
-    
     vector<vector<int>> subsets(vector<int>& nums) {
-        dfs(nums, 0);
+        vector<vector<int>> res;
+        vector<int> cur;
+        int size = 1 << nums.size();
+        for (int i=0; i<size; ++i) {
+            cur.clear();
+            for (int j=0; j<nums.size(); ++j) {
+                if (i&(1<<j)) {
+                    cur.push_back(nums[j]);
+                }
+            }
+            res.push_back(cur);
+        }
         return res;
     }
 };
