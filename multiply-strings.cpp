29fda773af0f99bab365f8204de5d05ccaf43ffc
liming-vie <liming.vie@gmail.com>
2017-10-07 21:51:01 +0800

    43. Multiply Strings; 2017/10/07

--- multiply-strings.cpp
+++ multiply-strings.cpp
@@ -2,29 +2,30 @@ class Solution {
 public:
     string multiply(string num1, string num2) {
         int l1=num1.length(), l2=num2.length();
-        for(int i=0; i<l1; ++i) num1[i]-='0';
-        for(int i=0; i<l2; ++i) num2[i]-='0';
+        vector<int> res(l1+l2, 0);
         
-        vector<int> res(l1+l2+1,0);
-        for(int i=l1-1; i>=0; --i) 
-            for(int j=l2-1; j>=0; --j) 
-                res[i+j+1]+=num1[i]*num2[j];
+        for (int i=0; i<l1; ++i) num1[i] -= '0';
+        for (int i=0; i<l2; ++i) num2[i] -= '0';
         
-        int t=0;
-        for(int i=l1+l2-1; i>=0; --i) {
-            res[i]+=t;
-            t=res[i]/10;
-            res[i]%=10;
+        for (int i=0; i<l1; ++i) {
+            for (int j=0; j<l2; ++j) {
+                res[i+j] += num1[l1-i-1] * num2[l2-j-1];
+            }
         }
         
-        string str="";
-        for(int i=l1+l2-1; i>=0; --i)
-            str=(char)('0'+res[i])+str;
-        
-        for(int i=0; i<l1+l2; ++i) {
-            if(str[i] != '0')
-                return str.substr(i);
+        int y=0;
+        for (int i=0; i<l1+l2; ++i) {
+            res[i] += y;
+            y=res[i] / 10;
+            res[i] %= 10;
         }
-        return "0";
+        
+        string str;
+        int i= l2+l1-1;
+        while(i>0 && !res[i])  --i;
+        for (; i>=0; --i) {
+            str+=(char)(res[i] + '0');
+        }
+        return str;
     }
 };
