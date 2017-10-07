6c2c5e1006e258c3840491b5873550fd095dc21e
liming-vie <liming.vie@gmail.com>
2017-10-07 22:08:58 +0800

    75. Sort Colors; three way partition; 2017/10/07

--- sort-colors.cpp
+++ sort-colors.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
-    // three way partition
     void sortColors(vector<int>& nums) {
-        if(nums.size()<2)   return;
-        
-        int i=0, l=0, r=nums.size()-1;
-        while(i<=r) {
-            if(nums[i]==1)    ++i;
-            else if(nums[i]>1)  swap(nums[i], nums[r--]);
-            else swap(nums[i++], nums[l++]);
+        int l=0, r=nums.size()-1;
+        int i=0;
+        while (i<=r) {
+            if (nums[i] == 1)   ++i;
+            else if (nums[i] == 2) swap(nums[i], nums[r--]);
+            else swap(nums[i++], nums[l++]);                                                      
         }
     }
 };
