2cc41ab438cb7eaf780bb9df2d625044fab6c8cc
liming-vie <liming.vie@gmail.com>
2017-10-07 19:14:26 +0800

    139. Word Break; O(n^2); 2017/10/07

--- word-break.cpp
+++ word-break.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-    	unordered_set<string> st;
-    	for(auto str:wordDict) 
-    		st.insert(str);
-    	
-        int len=s.length();
+        unordered_set<string> st;
+        for (const string& word : wordDict) {
+            st.insert(word);
+        }
+        
+        int len = s.length();
         vector<bool> dp(len+1, false);
-        dp[0]=true;
-        for(int i=1; i<=len; ++i) 
-        	for(int j=i; j>0; --j) {
-        		if(dp[j-1] && st.find(s.substr(j-1, i-j+1))!=st.end()) 
-        			dp[i]=true;
-        	}
+        dp[0] = true;
+        
+        for (int i=1; i<=len; ++i) {
+            for (int j=i; j>0; --j) {
+                if (dp[j-1] && st.find(s.substr(j-1, i-j+1))!=st.end()) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        
         return dp[len];
     }
 };
