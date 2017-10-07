adbab8f87afc113f22b6dcd4d9840b4100b016ba
liming-vie <liming.vie@gmail.com>
2017-10-07 20:55:56 +0800

    78. Subsets; backtracking; 2017/10/07

--- subsets.cpp
+++ subsets.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> res;
-        vector<int> cur;
-        int size=1<<nums.size();
-        for(int k=0; k<size; ++k) {
-            cur.clear();
-            for(int i=0; i<nums.size(); ++i) 
-                if((1<<i) & k)
-                    cur.push_back(nums[i]);
+    vector<vector<int>> res;
+    vector<int> cur;
+    
+    void dfs(const vector<int>& nums, int vi) {
+        if (vi == nums.size()) {
             res.push_back(cur);
+            return ;
         }
+        
+        dfs(nums, vi+1);
+        
+        cur.push_back(nums[vi]);
+        dfs(nums, vi+1);
+        cur.pop_back();
+    }
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+        dfs(nums, 0);
         return res;
     }
 };
