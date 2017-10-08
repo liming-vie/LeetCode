a6104f2ab22e54d947cb3b5cb2a97ca99d590238
liming-vie <liming.vie@gmail.com>
2017-10-08 21:30:56 +0800

    33. Search in Rotated Sorted Array; 2017/10/08

--- search-in-rotated-sorted-array.cpp
+++ search-in-rotated-sorted-array.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    #define MID(left, right) (left+((right-left)>>1))
-    int func(vector<int>&nums, int left, int right, int target) {
-        if(left>right)  return -1;
-        // binary search
-        if(nums[left] <= nums[right]) {
-            while(left<=right) {
-                int mid=MID(left, right);
-                if(nums[mid]==target)   return mid;
-                else if(nums[mid]>target)   right=mid-1;
-                else    left=mid+1;
+    int func(const vector<int>& nums, int target, int left, int right) {
+        if (left > right)   return -1;
+        
+        int mid = left + ((right - left) >> 1);
+        if (nums[mid] == target)    return mid;
+        
+        if (nums[right] < nums[left]) {
+            int tmp = func(nums, target, left, mid-1);
+            if (tmp >= 0)   return tmp;
+            return func(nums, target, mid+1, right);
+        } else {
+            while (left <= right) {
+                mid = left + ((right-left)>>1);
+                if (nums[mid] == target)    return mid;
+                else if (nums[mid] < target)    ++left;
+                else    --right;
             }
-            return -1;
         }
-        else {
-            int mid=MID(left, right);
-            int a=func(nums, left, mid, target);
-            if(a!=-1)   return a;
-            return func(nums, mid+1, right, target);
-        }
+        return -1;
     }
-
+    
     int search(vector<int>& nums, int target) {
-        return func(nums, 0, nums.size()-1, target);
+        return func(nums, target, 0, nums.size()-1);
     }
 };
