a4cf01532535a4ec3db18603b40598361f961a79
liming-vie <liming.vie@gmail.com>
2017-10-09 13:33:30 +0800

    88. Merge Sorted Array; 2017/10/09

--- merge-sorted-array.cpp
+++ merge-sorted-array.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int idx=m+n-1, i1=m-1, i2=n-1;
-        while(i1>=0 || i2>=0) {
-            if(i2<0 || (i1>=0 && nums1[i1]>nums2[i2])) {
-                nums1[idx--]=nums1[i1--];
+        int i=m-1, j=n-1, idx=m+n-1;
+        while (i>=0 || j>=0) {
+            if (j<0 || (i>=0 && nums1[i] >= nums2[j])) {
+                nums1[idx] = nums1[i--];
             } else {
-                nums1[idx--]=nums2[i2--];
+                nums1[idx] = nums2[j--];
             }
+            --idx;
         }
     }
 };
