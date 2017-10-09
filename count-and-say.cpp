9f2501a9f9cf0f1621376dc4c7979b1ab2ad3f31
liming-vie <liming.vie@gmail.com>
2017-10-09 12:59:43 +0800

    38. Count and Say; 2017/10/09

--- count-and-say.cpp
+++ count-and-say.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     string countAndSay(int n) {
-        if(n<=0)    return "";
-        string s1="1", s2="";
-        for(int i=1; i<n; ++i) {
-            int count=1;
-            int len=s1.length();
-            for(int i=1; i<=len; ++i) {
-                if(i<len && s1[i] == s1[i-1])
+        string s[2] = {"1"};
+        int idx=0, prev=1;
+        while (--n) {
+            swap(idx, prev);
+            s[idx].clear();
+            int count = 1;
+            for (int i=1; i<=s[prev].length(); ++i) {
+                if (i<s[prev].length() && s[prev][i] == s[prev][i-1]) {
                     ++count;
-                else {
-                    s2 += (count+'0');
-                    s2 += s1[i-1];
-                    count=1;
+                } else {
+                    s[idx] += (count + '0');
+                    s[idx] += s[prev][i-1];
+                    count = 1;
                 }
             }
-            s1=s2;
-            s2="";
         }
-        return s1;
+        return s[idx];
     }
 };
