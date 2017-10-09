e7f01fd5a6cc05537f26aec600fa503c59c13b8f
liming-vie <liming.vie@gmail.com>
2017-10-09 13:14:32 +0800

    49. Group Anagrams; 2017/10/09

--- group-anagrams.cpp
+++ group-anagrams.cpp
@@ -2,17 +2,19 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, vector<string>> mp;
-        for(auto str:strs) {
-            string s=str;
-            sort(str.begin(), str.end());
-            if(mp.find(str)==mp.end()) {
-                mp[str]=vector<string>();
+        for (const string& str : strs) {
+            string s = str;
+            sort(s.begin(), s.end());
+            if (mp.find(s) == mp.end()) {
+                mp[s] = vector<string>({str});
+            } else {
+                mp[s].push_back(str);
             }
-            mp[str].push_back(s);
         }
+        
         vector<vector<string>> res;
-        for(auto kp:mp) {
-            res.push_back(kp.second);
+        for (const auto& kv : mp) {
+            res.push_back(kv.second);
         }
         return res;
     }
