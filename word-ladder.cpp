08bc00f43ee1ec878594be07bd00a01d5fc960b4
liming-vie <liming.vie@gmail.com>
2017-10-10 21:52:57 +0800

    127. Word Ladder; queue; 2017/10/10

--- word-ladder.cpp
+++ word-ladder.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
-    int ladderLength(string beginWord, string endWord, unordered_set<string>& wordList) {
-        int res=1;
-        int len=beginWord.length();
-        queue<string> q;
-        q.push(beginWord);  q.push("");
-        while(true) {
-            auto t=q.front();
-            q.pop();
-            if(t!="") {
-                if(t==endWord)  return res;
-                for(int i=0; i<len; ++i) {
-                    char cur=t[i];
-                    for(int j='a'; j<='z'; ++j) {
-                        if(j==cur)  continue;
-                        t[i]=j;
-                        if(wordList.find(t)!=wordList.end()) {
-                            q.push(t);
-                            wordList.erase(t);
-                        }
-                    }
-                    t[i]=cur;
-                }    
-            } else {
-                if (q.empty())  return 0;
-                q.push("");
-                ++res;
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> st;
+        for (const auto& word : wordList)
+            st.insert(word);
+        st.erase(beginWord);
+        
+        queue<pair<string, int>> q;
+        q.push(make_pair(beginWord, 1));
+        while (!q.empty()) {
+            auto &kv = q.front();
+            for (int i=0; i<kv.first.length(); ++i) {
+                char ori = kv.first[i];
+                for (char c='a'; c<='z'; ++c) {
+                    if (c==ori)  continue;
+                    kv.first[i] = c;
+                    
+                    if (st.find(kv.first) == st.end())  continue;
+                    st.erase(kv.first);
+                    
+                    if (kv.first == endWord)
+                        return kv.second + 1;
+                    q.push(make_pair(kv.first, kv.second+1));
+                }
+                kv.first[i] = ori;
             }
+            q.pop();
         }
         return 0;
     }
