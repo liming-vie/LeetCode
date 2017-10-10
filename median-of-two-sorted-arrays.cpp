01d0241b075591bf84ba60e922e2dd9fedc67df9
liming-vie <liming.vie@gmail.com>
2017-10-10 19:18:52 +0800

    4. Median of Two Sorted Arrays; split into two set; 2017/10/10

--- median-of-two-sorted-arrays.cpp
+++ median-of-two-sorted-arrays.cpp
@@ -1,27 +1,34 @@
 class Solution {
 public:
-    double findKthNumber(int* nums1, int size1, int* nums2, int size2, int k) {
-        if(size1 > size2)   return findKthNumber(nums2, size2, nums1, size1, k);
-        if(size1 == 0)  return nums2[k-1];
-        if(k==1)    return *nums1 < *nums2 ? *nums1 : *nums2;
-        int mid1 = k >> 1;
-        mid1 = mid1 > size1 ? size1 : mid1;
-        int mid2 = k-mid1;
-        int n1 = nums1[mid1-1], n2 = nums2[mid2-1];
-        if(n1 < n2) 
-            return findKthNumber(nums1+mid1, size1-mid1, nums2, size2, k-mid1);
-        if(n1 > n2) 
-            return findKthNumber(nums1, size1, nums2+mid2, size2-mid2, k-mid2);
-        return n1;
-    }
-    
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int s1 = nums1.size();
-        int s2 = nums2.size();
-        int mid = (s1+s2)>>1;
-        if((s1 + s2) & 1){
-            return findKthNumber(nums1.data(), s1, nums2.data(), s2, mid + 1);
+        if (nums1.size() > nums2.size())    swap(nums1, nums2);
+        int n=nums1.size(), m=nums2.size();
+        
+        int l=0, r=n;
+        int half = (n+m+1)>>1;
+        while (l<=r) {
+            int vi=(l+r)>>1;
+            int vj=half-vi;
+            if (vi<n && nums1[vi] < nums2[vj-1]) {
+                l = vi+1;
+            } else if (vi>0 && nums1[vi-1] > nums2[vj]) {
+                r = vi-1;
+            } else {
+                double leftmax;
+                if (vi == 0)    leftmax = nums2[vj-1];
+                else if (vj==0) leftmax = nums1[vi-1];
+                else leftmax = max(nums2[vj-1], nums1[vi-1]);
+                
+                if ((n + m) & 1)    return leftmax;
+                
+                double rightmin;
+                if (vi==n)  rightmin = nums2[vj];
+                else if (vj==m) rightmin = nums1[vi];
+                else rightmin = min(nums1[vi], nums2[vj]);
+                
+                return (leftmax + rightmin)/2;
+            }
         }
-        return (findKthNumber(nums1.data(), s1, nums2.data(), s2, mid + 1) + findKthNumber(nums1.data(), s1, nums2.data(), s2, mid)) / 2;
+        return 0.0;
     }
 };
