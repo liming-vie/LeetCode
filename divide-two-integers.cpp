7e08e72bf192155e424b04c15ed8ff09631a4e30
liming-vie <liming.vie@gmail.com>
2017-10-10 15:20:33 +0800

    29. Divide Two Integers; 2017/10/10

--- divide-two-integers.cpp
+++ divide-two-integers.cpp
@@ -1,26 +1,27 @@
-#define MAX_INT ~(1<<31)
-#define MIN_INT 1<<31
+#define MAX_INT 2147483647
+#define MIN_INT -2147483648
+
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        long long end = dividend, sor = divisor;
-        end = end < 0 ? -end : end;
-        sor = sor < 0 ? -sor : sor;
-        long long int res = 0;
-        long long int k = 0, tmp = sor;
-        while (tmp <= end) {
-            tmp <<= 1;
+        long long ldividend = dividend < 0 ? -((long long)dividend) : dividend;
+        long long ldivisor = divisor < 0 ? -((long long)divisor) : divisor;
+        
+        int k=0;
+        while (ldividend >= (ldivisor << k)) {
             ++k;
         }
-        while (end >= sor) {
-            while (tmp > end) {
-                tmp >>= 1;
-                --k;
+        --k;
+        long long res = 0;
+        while (ldividend >= ldivisor) {
+            res += (1LL<<k);
+            ldividend -= (ldivisor << k);
+            while (ldividend < (ldivisor<<k)) {
+            --k;
             }
-            res += 1LL << k;
-            end -= tmp;
         }
-        res = ((dividend<0) ^ (divisor<0)) ? -res : res;
-        return (res<MIN_INT || res>MAX_INT) ? MAX_INT : res;
+        
+        res = ((dividend<0)^(divisor<0))?-res:res;
+        return (res > MAX_INT || res < MIN_INT) ? MAX_INT : res;
     }
 };
