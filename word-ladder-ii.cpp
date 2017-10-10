0a8130ae72266cfaece8130ec6ed3e72be022323
liming-vie <liming.vie@gmail.com>
2017-10-10 23:33:48 +0800

    126. Word Ladder II; use layer set instead of queue; 2017/10/10

--- word-ladder-ii.cpp
+++ word-ladder-ii.cpp
@@ -1,54 +1,63 @@
 class Solution {
 public:
-    unordered_map<string, vector<string>> pre;
+    unordered_map<int, vector<int>> pre;
     vector<vector<string>> res;
     vector<string> cur;
-
-    void generate(string str) {
-        cur.push_back(str);
-        if(!pre[str].size()) {
+    
+    vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
+        wordList.push_back(beginWord);
+        unordered_map<string, int> mp;
+        unordered_set<string> st;
+        for (int i=0; i<wordList.size(); ++i) {
+            mp[wordList[i]] = i;
+            st.insert(wordList[i]);
+        }
+        
+        unordered_set<string> layer[2];
+        int prei=0, curi=1;
+        layer[prei].insert(beginWord);
+        while(layer[prei].size()) {
+            for (const auto& str : layer[prei]) {
+                st.erase(str);
+            }
+            layer[curi].clear();
+            
+            for (const auto& str : layer[prei]) {
+                string tmp = str;
+                for (int i=0; i<str.length(); ++i) {
+                    for (char c='a'; c<='z'; ++c) {
+                        if (c==str[i])  continue;
+                        tmp[i] = c;
+                        
+                        if (st.find(tmp) == st.end()) 
+                            continue;
+                        pre[mp[tmp]].push_back(mp[str]);
+                        layer[curi].insert(tmp);
+                    }
+                    tmp[i] = str[i];
+                }
+            }
+            
+            if (layer[curi].find(endWord) != layer[curi].end()) break;
+            swap(prei, curi);
+        }
+        
+        if (!layer[prei].size())    return res;
+        generate(wordList, mp[endWord]);
+        return res;
+    }
+    
+    
+    void generate(const vector<string>& wordList, int vi) {
+        cur.push_back(wordList[vi]);
+        if (pre[vi].size()) {
+            for (int i : pre[vi])
+                generate(wordList, i);
+        } else {
             vector<string> tmp(cur);
             reverse(tmp.begin(), tmp.end());
             res.push_back(tmp);
-        } else {
-            for(auto s:pre[str]) {
-                generate(s);
-            }
         }
         cur.pop_back();
-    }
-
-    vector<vector<string>> findLadders(string beginWord, string endWord, unordered_set<string> &wordList) {
-        wordList.insert(endWord);
-        unordered_set<string> layer[2]; // instead of queue
-        layer[0].insert(beginWord);
-        int curi=1, prei=0;
-        int len=beginWord.size();
-        while(layer[prei].size()) {
-            for(auto str : layer[prei]) {
-                wordList.erase(str);
-            }
-            layer[curi].clear();
-            for(auto str:layer[prei]) {
-                for(int i=0; i<len; ++i) {
-                    string tmp=str;
-                    char c=str[i];
-                    for(int j='a'; j<='z'; ++j) {
-                        if(c==j)    continue;
-                        str[i]=j;
-                        if(wordList.find(str)!=wordList.end()) {
-                            layer[curi].insert(str);
-                            pre[str].push_back(tmp);
-                        }
-                    }
-                    str[i]=c;
-                }
-            }
-            if(layer[curi].find(endWord)!=layer[curi].end())    break;
-            curi = !curi; prei = !prei;
-        }
-        if(!layer[prei].size()) return res;
-        generate(endWord);
-        return res;
     }
 };
