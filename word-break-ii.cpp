d5e58ce96252a0ddc0dde11bd8e4715f9a73ae02
liming-vie <liming.vie@gmail.com>
2017-10-10 21:30:05 +0800

    140. Word Break II; dp; 2017/10/10

--- word-break-ii.cpp
+++ word-break-ii.cpp
@@ -1,53 +1,47 @@
 class Solution {
 public:
-	vector<vector<char>> dp; // str.substr(i,j-i+1)
-	vector<string> res;
-	int len;
-	string str;
-
+    vector<vector<int>> dp;
     vector<string> wordBreak(string s, vector<string>& wordDict) {
         unordered_set<string> st;
-        for(auto tmp:wordDict) st.insert(tmp);
-
-        len=s.length();
-        dp=vector<vector<char>>(len, vector<char>(len, 0));
+        for (const string& word : wordDict)
+            st.insert(word);
         
-  		for(int i=0; i<len; ++i) {
-  			for(int j=i; j>=0; --j) {
-  				if (st.find(s.substr(j,i-j+1))!=st.end()) {
-  					dp[j][i]=1;
-  					continue;
-  				}
-  				for(int k=j; k<i; ++k) {
-  					if(dp[j][k] && dp[k+1][i]) {
-  						dp[j][i]=2;
-  						break;
-  					}
-  				}
-  			}
-  		}
-
-  		if(dp[0][len-1]==0)	return res;
-
-  		str=s;
-  		search("", 0);
-
-  		return res;
+        int len=s.length();
+        dp = vector<vector<int>>(len, vector<int>(len, 0));
+        for (int i=0; i<len; ++i) {
+            for (int j=i; j>=0; --j) {
+                if (st.find(s.substr(j,i-j+1)) != st.end()) {
+                    dp[j][i] = 1;
+                } else {
+                    for (int k=j; k<i; ++k) {
+                        if (dp[j][k] && dp[k+1][i]) {
+                            dp[j][i] = 2;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        
+        if (!dp[0][len-1])  return res;
+        
+        search(s, "", 0);
+        return res;
     }
-
-    void search(string cur, int vi) {
-    	if(vi==len)	{
-    		res.push_back(cur);
-    		return;
-    	}
-
-    	for(int i=vi; i<len; ++i) {
-    		if(dp[vi][i]==1) {
-    			string tmp=cur;
-    			if(tmp.length()!=0)	tmp+=" ";
-    			tmp += str.substr(vi, i-vi+1);
-    			search(tmp, i+1);
-    		}
-    	}
+    
+    vector<string> res;
+    void search(const string& str, const string& cur, int vi) {
+        if (vi == str.length()) {
+            res.push_back(cur);
+            return;
+        }
+        
+        string c = cur.length() ? " " : "";
+        for (int i=vi; i<str.length(); ++i) {
+            if (dp[vi][i] == 1) {
+                search(str, cur+c+str.substr(vi, i-vi+1), i+1);
+            }
+        }
     }
+    
 };
