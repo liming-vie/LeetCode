2de3da8964b8b6d0383d0400432254bc7694bdd7
liming-vie <liming.vie@gmail.com>
2017-10-10 17:04:55 +0800

    380. Insert Delete GetRandom O(1); 2017/10/10

--- insert-delete-getrandom-o1.cpp
+++ insert-delete-getrandom-o1.cpp
@@ -2,43 +2,40 @@ class RandomizedSet {
 public:
     /** Initialize your data structure here. */
     RandomizedSet() {
-        srand((unsigned)time(NULL)); 
-        vals_.reserve(100);
+        
     }
     
     /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
     bool insert(int val) {
-        if (index_.find(val) != index_.end()) {
-            return false;
-        }
-        index_[val] = vals_.size();
-        vals_.push_back(val);
+        if (mp.find(val) != mp.end())   return false;
+        mp[val] = nums.size();
+        nums.push_back(val);
         return true;
     }
     
     /** Removes a value from the set. Returns true if the set contained the specified element. */
     bool remove(int val) {
-        if (index_.find(val) == index_.end()) {
-            return false;
+        if (mp.find(val) == mp.end())   return false;
+        
+        int n=nums.size();
+        int idx = mp[val];
+        mp.erase(val);
+        if (idx < n-1) {
+            int v=nums[n-1];
+            nums[idx] = v;
+            mp[v] = idx;
         }
-        int n=vals_.size() - 1;
-        int last = vals_[n];
-        int idx = index_[val];
-        vals_[idx] = last;
-        index_[last] = idx;
-        index_.erase(val);
-        vals_.resize(n);
+        nums.resize(n-1);
         return true;
     }
     
     /** Get a random element from the set. */
     int getRandom() {
-        return vals_[rand() % vals_.size()];
+        return nums[rand()%nums.size()];
     }
     
-private:
-    vector<int> vals_;
-    map<int,int> index_;
+    unordered_map<int, int> mp;
+    vector<int> nums;
 };
 
 /**
