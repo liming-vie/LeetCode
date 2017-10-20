19bacf5f00271ba2ef990d5e185ab7a14ff23e6b
liming-vie <liming.vie@gmail.com>
2017-10-20 16:52:40 +0800

    28. Implement strStr(); 2017/10/20

--- implement-strstr.cpp
+++ implement-strstr.cpp
@@ -1,29 +1,36 @@
 class Solution {
 public:
-    vector<int> next;
-    
-    void func(string pattern) {
-        int len=pattern.length();
-        next.resize(len+1);
-        next[0]=-1;
-        int k=-1,j=0;
-        while(j<len) {
-            if(k<0 || pattern[j]==pattern[k]) {
-                ++j,++k;
-                next[j]=(pattern[j]==pattern[k]?next[k]:k);
-            }
-            else k=next[k];
+    vector<int> getNext(const string& p) {
+        int len=p.length();
+        int k=-1;
+        vector<int> next(len);
+        next[0] = -1;
+        int i=0;
+        while (i<len) {
+            if (k>=0 && p[k]!=p[i])
+                k=next[k];
+            if (++i >= len) break;
+            ++k;
+            next[i] = (p[k] == p[i]) ? next[k] : k;
         }
+        return next;
     }
-
+    
     int strStr(string haystack, string needle) {
-        func(needle);
-        int l1=haystack.length(), l2=needle.size();
+        int l1=haystack.length(), l2=needle.length();
+        if (l1 < l2)    return -1;
+        if (l2==0)  return 0;
+        
+        const auto& next = getNext(needle);
         int i=0, j=0;
-        while(i<l1 && j<l2) {
-            if(j<0||haystack[i]==needle[j]) ++i,++j;
-            else    j=next[j];
+        
+        while (i<l1 && j<l2) {
+            if (j==-1 || haystack[i]!=needle[j]) {
+                j=next[j];
+            }
+            ++i;
+            ++j;
         }
-        return j==l2 ? i-j : -1;
+        return j==l2 ? (i-l2) : -1;
     }
 };
