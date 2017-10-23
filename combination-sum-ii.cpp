77f4d0a28769ebf13ac3425cc475dc2b4e6be87d
liming-vie <liming.vie@gmail.com>
2017-10-23 23:17:17 +0800

    40. Combination Sum II; 2017/10/23

--- combination-sum-ii.cpp
+++ combination-sum-ii.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> res;
-    vector<bool> use;
+    vector<int> cur;
     
-    void func(vector<int>& nums, int vi, vector<int>& cur, int sum, int target) {
-        if(sum > target)    return;
-        if(vi == nums.size()) {
-            if(sum==target)
-                res.push_back(cur);
+    void func(const vector<int>& nums, int vi, int target, bool last) {
+        if (target < 0) {
             return;
         }
-        // not put this number
-        func(nums, vi+1, cur, sum, target);
-        // only when thr previous one being used, the same one can be use
-        if(vi && nums[vi] == nums[vi-1] && !use[vi-1])  return;
-        // put once
-        use[vi]=true;
+        if (vi==nums.size()) {
+            if (target == 0) {
+                res.push_back(cur);
+            } 
+            return;
+        }
+        
+        if (!(vi && nums[vi]==nums[vi-1] && last)) {
+            func(nums, vi+1, target, false);
+        }
+        
         cur.push_back(nums[vi]);
-        func(nums, vi+1, cur, sum+nums[vi], target);
+        func(nums, vi+1, target-nums[vi], true);
         cur.pop_back();
-        use[vi]=false;
     }
+    
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         sort(candidates.begin(), candidates.end());
-        use.resize(candidates.size(), false);
-        vector<int> cur;
-        func(candidates, 0, cur, 0, target);
+        
+        func(candidates, 0, target, false);
+        
         return res;
     }
 };
