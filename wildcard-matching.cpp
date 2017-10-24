594b7e49f45dc35b23ca02f3ea66ca3f70c88b60
liming-vie <liming.vie@gmail.com>
2017-10-24 19:46:26 +0800

    44. Wildcard Matching; dp; 2017/10/24

--- wildcard-matching.cpp
+++ wildcard-matching.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        int l1=s.length(), l2=p.length();
-        vector<vector<bool>> dp(l1+1, vector<bool>(l2+1, false));
+        int ls=s.length(), lp=p.length();
+        vector<vector<bool>> dp(ls+1, vector<bool>(lp+1, false));
+        
         dp[0][0]=true;
-        for(int i=0; i<l2; ++i) {
-            if(p[i]=='*')   dp[0][i+1]=true;
-            else    break;
-        }
-        for(int i=1; i<=l1; ++i) {
-            for(int j=1; j<=l2; ++j) {
-                if(s[i-1]==p[j-1] || p[j-1]=='?') dp[i][j]=dp[i-1][j-1];
-                else if(p[j-1]=='*')  dp[i][j] = (dp[i-1][j]||dp[i][j-1]);
+        for(int i=0; i<lp && p[i]=='*'; ++i)
+            dp[0][i+1]=true;
+        
+        for (int i=0; i<ls; ++i) {
+            for (int j=0; j<lp; ++j) {
+                if (s[i]==p[j] || p[j]=='?') dp[i+1][j+1]=dp[i][j];
+                else if (p[j]=='*') dp[i+1][j+1] = dp[i+1][j] || dp[i][j+1];
             }
         }
-        return dp[l1][l2];
+        
+        return dp[ls][lp];
     }
 };
