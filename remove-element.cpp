91f3c85da5039e76cab90d49b40030cfae809b17
liming-vie <liming.vie@gmail.com>
2017-10-25 18:46:18 +0800

    27. Remove Element; 2017/10/25

--- remove-element.cpp
+++ remove-element.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int n=nums.size(), idx=0;
-        for(int i=0; i<n; ++i) {
-            if(nums[i]!=val) 
-                nums[idx++]=nums[i];
+        int tail=0;
+        for (int i=0; i<nums.size(); ++i) {
+            if (nums[i]!=val)   nums[tail++]=nums[i];
         }
-        return idx;
+        return tail;
     }
 };
