89b841496f41e4a3d67d56e57345fdf0670062da
liming-vie <liming.vie@gmail.com>
2017-10-25 13:26:23 +0800

    50. Pow(x, n); 2017/10/25

--- powx-n.cpp
+++ powx-n.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        if(n==0)    return 1.0;
-        double tmp=myPow(x, abs(n/2));
-        tmp *= tmp;
-        if(n&1) tmp *= x;
-        if(n<0) tmp=1.0/tmp;
-        return tmp;
+        if (n==0)   return 1.;
+
+        long t = n < 0 ? -(long)n : n;
+        
+        double ans = 1;
+        while (t) {
+            if (t&1)    ans *= x;
+            x*=x;
+            t>>=1;
+        }
+        return n<0 ? 1./ans : ans;
     }
 };
