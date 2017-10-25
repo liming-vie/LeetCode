154295ad23792071358301c4ec15d25b70913f33
liming-vie <liming.vie@gmail.com>
2017-10-25 17:08:32 +0800

    69. Sqrt(x); binary search; 2017/10/25

--- sqrtx.cpp
+++ sqrtx.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        int l=0, r=x;
-        unsigned long long mid,tmp;
-        while(l<=r) {
-            mid=(l+r)>>1;
-            tmp=mid*mid;
-            if(tmp < x)
-                l=mid+1;
-            else if(tmp>x) 
-                r=mid-1;
-            else
-                return mid;
+        int l=0, r=x, mid;
+        long long tmp;
+        while (l<=r) {
+            mid = l+((r-l)>>1);
+            tmp=(long long)mid*mid;
+            if (tmp == x)   return mid;
+            if (tmp < x)    l=mid+1;
+            else    r=mid-1;
         }
         return r;
     }
