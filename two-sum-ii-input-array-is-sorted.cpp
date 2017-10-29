ae9eda9c44c2c5159f841580529842b86e76086d
liming-vie <liming.vie@gmail.com>
2017-10-29 17:57:01 +0800

    167. Two Sum II - Input array is sorted; two pointer; 2017/10/29

--- two-sum-ii-input-array-is-sorted.cpp
+++ two-sum-ii-input-array-is-sorted.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        int l=0, r=numbers.size()-1, sum;
-        while(l<r) {
-            sum=numbers[l]+numbers[r];
-            if (sum<target) ++l;
-            else if (sum>target) --r;
-            else return vector<int>{l+1, r+1};
+        int l=0, r=numbers.size()-1;
+        while (l<r) {
+            int sum=numbers[l]+numbers[r];
+            if (sum < target)   ++l;
+            else if (sum>target)--r;
+            else return vector<int>({l+1,r+1});
         }
+        return vector<int>();
     }
 };
