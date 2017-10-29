a49a5828308762b3372eedbe29683be0a2dab170
liming-vie <liming.vie@gmail.com>
2017-10-29 23:11:10 +0800

    123. Best Time to Buy and Sell Stock III; dp; 2017/10/29

--- best-time-to-buy-and-sell-stock-iii.cpp
+++ best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,27 +1,36 @@
-#define MAX(a,b) ((a)>(b)?(a):(b))
-#define MIN(a,b) ((a)<(b)?(a):(b))
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        if(n<2) return 0;
+        if (n<2)    return 0;
         
-        vector<int> dp1(n+1,0), dp2(n+1,0);
-        int max=prices[n-1];
-        for(int i=n-1; i>=1; --i) {
-            max=MAX(max, prices[i-1]);
-            int tmp=max-prices[i-1];
-            dp1[i]=MAX(tmp, dp1[i+1]);
+        vector<int> first(n+1);
+        first[0]=first[1]=0;
+        int cmin=prices[0];
+        for (int i=1; i<n; ++i) {
+            if (cmin < prices[i]) {
+                first[i+1] = max(first[i], prices[i]-cmin);
+            } else {
+                cmin = prices[i];
+                first[i+1] = first[i];
+            }
         }
-        int min=prices[0];
-        for(int i=2; i<=n; ++i) {
-            min=MIN(min, prices[i-1]);
-            int tmp=prices[i-1]-min;
-            dp2[i]=MAX(tmp, dp2[i-1]);
+        
+        vector<int> second(n+2);
+        second[n] = second[n+1] = 0;
+        int cmax=prices[n-1];
+        for (int i=n-2; i>=0; --i) {
+            if (cmax > prices[i]) {
+                second[i+1] = max(second[i+2], cmax-prices[i]);
+            } else {
+                cmax = prices[i];
+                second[i+1]=second[i+2];
+            }
         }
+        
         int res=0;
-        for(int i=1; i<=n; ++i) {
-            res=MAX(res, dp1[i]+dp2[i-1]);
+        for (int i=1; i<=n+1; ++i) {
+            res = max(res, first[i-1]+second[i]);
         }
         return res;
     }
