0ad2e2ab44ae54a5ff8dd17219b8a712c2ae2b65
liming-vie <liming.vie@gmail.com>
2017-10-30 22:34:25 +0800

    45. Jump Game II; greedy; 2017/10/30

--- jump-game-ii.cpp
+++ jump-game-ii.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int max=0;
-        int curm=0;
-        int step=0;
-        int n=nums.size();
-        for(int i=0; i<n; ++i){
-            if(curm<i){
-                ++step;
-                curm=max;
-                if(curm>=n-1)   return step;
+        int res=0, cur=0, next=0, n=nums.size();
+        for (int i=0; i<n; ++i) {
+            if (cur < i) {
+                ++res;
+                cur=next;
             }
-            int tmp=nums[i]+i;
-            max=max<tmp?tmp:max;
+            next = max(next, i+nums[i]);
         }
-        return 0;
+        return res;
     }
 };
