687826227f88ac654035522529b2ab4bee46eedc
liming-vie <liming.vie@gmail.com>
2017-10-31 21:33:02 +0800

    152. Maximum Product Subarray; dp; 2017/10/31

--- maximum-product-subarray.cpp
+++ maximum-product-subarray.cpp
@@ -1,20 +1,16 @@
-#define MAX(a,b)	((a)>(b)?(a):(b))
-#define MIN(a,b)	((a)<(b)?(a):(b))
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int n=nums.size();
-        if(!n)  return 0;
-        
+        int n=nums.size(); if (!n) return 0;
+        int maxp=nums[0], minp=nums[0];
         int res=nums[0];
-        int min=nums[0], max=nums[0];
-        for(int i=1; i<n; ++i) {
-            int t1=min*nums[i];
-            int t2=max*nums[i];
-            min=MIN(nums[i], MIN(t1,t2));
-            max=MAX(nums[i], MAX(t1,t2));
-            res=MAX(max, res);
+        for (int i=1; i<n; ++i) {
+            int t1=minp*nums[i];
+            int t2=maxp*nums[i];
+            minp = min(min(t1, t2), nums[i]);
+            maxp = max(max(t1, t2), nums[i]);
+            res = max(res, maxp);
         }
         return res;
-    }
+    } 
 };
