faa029047c62523609aa665f3202a116c84ca65f
liming-vie <liming.vie@gmail.com>
2017-10-31 17:55:43 +0800

    41. First Missing Positive; 2017/10/31

--- first-missing-positive.cpp
+++ first-missing-positive.cpp
@@ -1,19 +1,16 @@
-// label the exist index number as minus
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
         int n=nums.size();
-        for(int i=0; i<n; ++i) {
-            if(nums[i]<=0)
-                nums[i] = n+1;
-        }
-        for(int i=0; i<n; ++i) {
-            int v=abs(nums[i]);
-            if(v>0 && v<=n && nums[v-1] > 0)
-                nums[v-1]=-nums[v-1];
-        }
-        for(int i=0; i<n; ++i) 
-            if(nums[i] > 0)
+        for (int i=0; i<n; ++i)
+            if (nums[i]<=0) nums[i]=n+1;
+        for (int i=0; i<n; ++i)
+            if (abs(nums[i]) <= n) {
+                int idx = abs(nums[i])-1;
+                nums[idx] = -abs(nums[idx]);
+            }
+        for (int i=0; i<n; ++i)
+            if (nums[i] > 0)
                 return i+1;
         return n+1;
     }
