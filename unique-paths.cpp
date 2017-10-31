f833ef90bb0600bfd86a93006c9ef36c2f3e8c04
liming-vie <liming.vie@gmail.com>
2017-10-31 21:14:27 +0800

    62. Unique Paths; dp; 2017/10/31

--- unique-paths.cpp
+++ unique-paths.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<int> dp(n+1, 0);
-        dp[1]=1;
-        for(int i=0; i<m; ++i) {
-            for(int j=1; j<=n; ++j) {
+        vector<int> dp(m, 1);
+        for (int i=1; i<n; ++i) {
+            for (int j=1; j<m; ++j)
                 dp[j] += dp[j-1];
-            }
         }
-        return dp[n-1];
+        return dp[m-1];
     }
 };
