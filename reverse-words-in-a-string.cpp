375154cfbc4a489dc4f1e2a8e7fc0ed1f9b3b30c
liming-vie <liming.vie@gmail.com>
2017-11-07 12:57:06 +0800

    151. Reverse Words in a String; 2017/11/07

--- reverse-words-in-a-string.cpp
+++ reverse-words-in-a-string.cpp
@@ -1,35 +1,24 @@
 class Solution {
 public:
-	// reverse characters for s[begin:end)
-	void myswap(string&s, int begin, int end) {
-		int k = (begin + end) >> 1;
-		for (int i = begin; i<k; ++i)
-			swap(s[i], s[end - i + begin - 1]);
-	}
-	void reverseWords(string &s) {
-		// remove duplicate blank
-		int len = s.length();
-		int curn = 0;
-		for (int i = 0; i < len; ++i) {
-			if (s[i] != ' ')	s[curn++] = s[i];
-			else if (curn && s[curn - 1] != ' ')	s[curn++] = ' ';
-		}
-		if (curn && s[curn - 1] == ' ')	--curn;
-		s.resize(curn);
-		len = curn;
-		// reverse characters in each word
-		// the sky is blue => eht yks si uelb
-		int vi = 0;
-		for (int i = 0; i<len; ++i) {
-			if (s[i] == ' ') {
-				myswap(s, vi, i);
-				vi = i + 1;
-			}
-		}
-		// reverse the last word
-		myswap(s, vi, len);
-		// reverse all characters in the string
-		// eht yks si uelb => blue is sky the
-		myswap(s, 0, len);
-	}
+    void reverseWords(string &s) {
+        int len=s.length(), idx=0;
+        for (int i=0; i<len; ++i) {
+            if (s[i]!=' ')  s[idx++]=s[i];
+            else if (i && s[i-1]!=' ')  s[idx++]=s[i];
+        }
+        len = idx;
+        while (len && s[len-1]==' ') --len;
+        s.resize(len);
+        
+        if (!len)    return;
+        
+        int start=0;
+        for (int i=0; i<=len; ++i) {
+            if (i==len || s[i]==' ') {
+                reverse(s.begin()+start, s.begin()+i);
+                start=i+1;
+            }
+        }
+        reverse(s.begin(), s.end());
+    }
 };
