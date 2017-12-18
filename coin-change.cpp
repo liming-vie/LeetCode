44ef8eb802c246490dc557432b120dfe1aeabfa6
liming-vie <liming.vie@gmail.com>
2017-12-18 11:41:46 +0800

    322. Coin Change; dp; 2017/12/18

--- coin-change.cpp
+++ coin-change.cpp
@@ -1,20 +1,17 @@
-#define min(a,b) a<b?a:b
-
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        int max=~(1<<31);
-        int* dp = new int[amount+1];
-        fill_n(dp+1, amount, max);
-        dp[0] = 0;
-        int n=coins.size();
-        for(int i=0; i<n; ++i) {
-            for(int j=coins[i]; j<=amount; ++j) {
-                int a=dp[j], b=dp[j-coins[i]];
-                dp[j] = b == max ? a : min(a, b + 1);
+        vector<int> dp(amount+1, 0);
+        dp[0]=1;
+        
+        for (int n:coins) {
+            for (int i=0; i<=amount-n; ++i) {
+                if (!dp[i])  continue;
+                else if (!dp[i+n] || dp[i]+1<dp[i+n])
+                    dp[i+n] = dp[i]+1;
             }
         }
         
-        return dp[amount] == max? -1 : dp[amount];
+        return dp[amount]-1;
     }
 };
