76fdf77757f896ed7cb6fa15e80d36add09a3b75
liming-vie <liming.vie@gmail.com>
2018-03-30 15:03:11 +0800

    280. Wiggle Sort; O(n) swap; 2018/03/30

--- wiggle-sort.cpp
+++ wiggle-sort.cpp
@@ -1,21 +1,10 @@
 class Solution {
 public:
     void wiggleSort(vector<int>& nums) {
-        int n=nums.size();
-        if (n<2)    return;
-        
-        auto mid_ptr = nums.begin() + (n>>1);
-        nth_element(nums.begin(), mid_ptr, nums.end());
-        int mid=*mid_ptr;
-        
-        #define A(i) nums[(1+(i<<1))%(n|1)]
-        int l=0, r=n-1, i=0;
-        while (i<=r) {
-            if (A(i)>mid) {
-                swap(A(i++), A(l++));
-            } else if (A(i)<mid) {
-                swap(A(i), A(r--));
-            } else ++i;
+        for (int i=1; i<nums.size(); ++i) {
+        	if ((i&1) == nums[i]<nums[i-1]) {
+        		swap(nums[i], nums[i-1]);
+        	}
         }
     }
 };
