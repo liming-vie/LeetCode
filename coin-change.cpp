14b6a0ecaebe080994dabd1961c40fd9ca72d5f7
liming-vie <liming.vie@gmail.com>
2018-04-01 22:43:32 +0800

    322. Coin Change; dp; 2018/04/01

--- coin-change.cpp
+++ coin-change.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount+1, 0);
-        dp[0]=1;
-        
-        for (int n:coins) {
-            for (int i=0; i<=amount-n; ++i) {
-                if (!dp[i])  continue;
-                else if (!dp[i+n] || dp[i]+1<dp[i+n])
-                    dp[i+n] = dp[i]+1;
+        vector<int> dp(amount+1, amount+1);
+        dp[0] = 0;
+        for (int i=1; i<=amount; ++i) {
+            for (int j=0; j<coins.size(); ++j) {
+                if (coins[j] > i)   break;
+                dp[i] = min(dp[i], dp[i-coins[j]]+1);
             }
         }
-        
-        return dp[amount]-1;
+        return dp[amount]<=amount ? dp[amount]:-1;
     }
 };
