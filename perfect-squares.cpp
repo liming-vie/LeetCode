57b5699ca1b0e3427e65618cd0901e37b46190e0
liming-vie <liming.vie@gmail.com>
2018-04-01 22:47:06 +0800

    279. Perfect Squares; dp; 2018/04/01

--- perfect-squares.cpp
+++ perfect-squares.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int numSquares(int n) {
-        if (n==0)   return 0;
-        
         vector<int> nums;
         for (int i=1; i<=sqrt(n); ++i)
             nums.push_back(i*i);
         
-        static vector<int> dp{0};
-        while (dp.size()<=n) {
-            int i=dp.size(), dp_i=n+1;
-            for (int j=0; nums[j]<=i && j<nums.size(); ++j) {
-                dp_i = min(dp_i, dp[i-nums[j]]+1);
+        vector<int> dp(n+1, n+1);
+        dp[0] = 0;
+        
+        for (int num:nums) {
+            for (int i=num; i<=n; ++i) {
+                dp[i] = min(dp[i], dp[i-num]+1);
             }
-            dp.push_back(dp_i);
         }
         
         return dp[n];
