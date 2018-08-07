c7e4502d3d23a3a33a7da36dbbc7d4c0c8ef2d7c
Liming Huang <limingvie@fb.com>
2018-08-07 19:44:50 +0100

    159. Longest Substring with At Most Two Distinct Characters; 2018/08/07

--- longest-substring-with-at-most-two-distinct-characters.cpp
+++ longest-substring-with-at-most-two-distinct-characters.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int lengthOfLongestSubstringTwoDistinct(string s) {
-        unordered_map<char, int> count;
+        unordered_map<char, int> mp;
         int start=0, res=0;
-        int cur_size=0;
         for (int i=0; i<s.length(); ++i) {
-            if (count[s[i]]==0) {
-                if (cur_size==2) {
-                    res = max(i-start, res);
-                    while (--count[s[start++]]);
-                } else ++cur_size;
+            if (mp.find(s[i])==mp.end() && mp.size()==2) {
+                while(start<i && --mp[s[start++]]);
+                mp.erase(s[start-1]);
             }
-            ++count[s[i]];
+            ++mp[s[i]];
+            res=max(res, i-start+1);
         }
-        return max((int)s.length()-start, res);
+        return max(res, (int)s.length()-start);
     }
 };
