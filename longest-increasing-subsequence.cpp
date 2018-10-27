dd8f24fd7d01f5f9ed2bddf7313960d6eadab5c3
liming-vie <liming.vie@gmail.com>
2018-10-27 22:35:20 +0800

    300. Longest Increasing Subsequence; set; 2018/10/27

--- longest-increasing-subsequence.cpp
+++ longest-increasing-subsequence.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
-        if (n<2) return n;
-        
-        int res=1;
-        vector<int> dp(n, 1);
-        for (int i=1; i<n; ++i) {
-            for (int j=i-1; j>=0; --j) {
-                if (nums[j]<nums[i]) {
-                    dp[i] = max(dp[i], dp[j]+1);
-                }
+        set<int> st;
+        for (int n:nums) {
+           auto l=st.lower_bound(n) ;
+            if (l!=st.end()) {
+                st.erase(l);
             }
-            res=max(res, dp[i]);
+            st.insert(n);
         }
-        return res;
+        return st.size();
     }
 };
